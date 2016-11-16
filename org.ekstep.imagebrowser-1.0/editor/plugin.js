c377ef49cda17e2c338bf65074015c07fef16dbb
sunil <sunils@ilimi.in>
2016-11-16 12:53:58 +0530

    #17 feat: image browser
    Changes:
    - added search images based on search text
    - added ui changes

--- org.ekstep.imagebrowser-1.0/editor/plugin.js
+++ org.ekstep.imagebrowser-1.0/editor/plugin.js
@@ -11,61 +11,109 @@ EkstepEditor.basePlugin.extend({
                 if (EkstepEditor.jQuery("i").hasClass("fa-image-selected")) {
                     EkstepEditor.jQuery("i").removeClass("fa-image-selected");
                 }
-                EkstepEditor.jQuery('#' + "imageOverlay-" + id).addClass("fa-image-selected");                
+                EkstepEditor.jQuery('#' + "fa-check-" + id).addClass("fa-image-selected");
             }
         };
     },
-    getImageFromServer: function() {
-        var instance = this;
-        var iservice = new EkstepEditor.iService();
-        var requestObj = {
+    getImageFromServer: function(searchText) {
+        var instance = this,
+            iservice = new EkstepEditor.iService(),
+            requestObj,
+            requestHeaders;
+
+        requestObj = {
             "request": {
                 "filters": {
-                    "objectType": ["Content"],
-                    "contentType": ["asset"]
+                    "mediaType": ["image"],
+                    "license": ["Creative Commons Attribution (CC BY)"],
+                    "name": []
                 }
             }
         };
-        var requestHeaders = {
+
+        requestHeaders = {
             headers: {
                 'Content-Type': 'application/json',
                 'user-id': 'ATTool'
             }
         };
 
+        _.isUndefined(searchText) ? null : requestObj.request.filters.name = [searchText];
+
         iservice.http.post(EkstepEditor.configService.searchServiceBaseUrl + 'v2/search', requestObj, requestHeaders).then(function(res) {
             instance.appendImageTag(res.data.result.content);
         });
     },
     appendImageTag: function(asset) {
+        var assetArea = "<div id=\"assetArea\" class=\"row\">";
+        assetArea += "<\/div>";
+        EkstepEditor.jQuery('#assetArea').remove();
+        EkstepEditor.jQuery('#assetWindow').append(assetArea);
+        EkstepEditor.jQuery('#selectanduse').addClass('disabled');
+
+        if (_.isUndefined(asset)) {
+            var strVar = "<div uib-alert class=\"alert-warning\" style=\"font-size: 30px; text-align: center;\">";
+            strVar += "<i class=\"fa fa-exclamation-triangle fa-1x\" aria-hidden=\"true\"><\/i>  Image not found";
+            strVar += "<\/div>";
+            EkstepEditor.jQuery('#assetArea').append(strVar);
+        }
+
         _.forEach(asset, function(obj, index) {
-            var imageElement = "<img class=\"img-thumbnail\"" + "id=" + index + " src=" + obj.downloadUrl + " onclick=\"window.org_ekstep_image_1_0.ImageSource(event, src, id);\" width=\"120\" height=\"120\">";
-            imageElement += "<i " + "id=imageOverlay-" + index + " class=\"fa fa-check-square-o fa-2x-custom\" aria-hidden=\"true\"><\/i>";
-            EkstepEditor.jQuery('#assestArea').append(imageElement);
+            var imageElement = "<img class=\"img-thumbnail\"" + "id=img-" + index + " src=" + obj.downloadUrl + " onclick=\"window.org_ekstep_image_1_0.ImageSource(event, src, id);\" width=\"120\" height=\"120\">";
+            imageElement += "<i " + "id=fa-check-img-" + index + " class=\"fa fa-check-square-o fa-2x-custom\" aria-hidden=\"true\"><\/i>";
+            EkstepEditor.jQuery('#assetArea').append(imageElement);
         });
+
+
     },
     showAssetBrowser: function() {
-        var instance = this;
-        var uibConfig = {
+        var instance = this,
+            uibConfig,
+            imageBrowserPopUp,
+            uibModalInstance,
+            searchText;
+
+
+        uibConfig = {
             modal_header: this.popupHTML().modal_header(),
             modal_content: this.popupHTML().modal_content(),
             modal_footer: this.popupHTML().modal_footer(),
             size: 'sm'
         };
 
-        var imageBrowserPopUp = new EkstepEditor.popupService();
+        imageBrowserPopUp = new EkstepEditor.popupService();
 
-        var uibModalInstance = imageBrowserPopUp.open(uibConfig);
+        uibModalInstance = imageBrowserPopUp.open(uibConfig);
 
         uibModalInstance.rendered.then(function() {
             //close popup window
             EkstepEditor.jQuery('#closePopUp').click(function() {
+                EkstepEditor.eventManager.dispatchEvent("imagebrowser:close");
                 uibModalInstance.close('cancel');
             });
 
             EkstepEditor.jQuery('#selectanduse').click(function() {
                 EkstepEditor.eventManager.dispatchEvent("imagebrowser:selected", EkstepEditor.jQuery(this).attr('src'));
                 uibModalInstance.close('cancel');
+            });
+
+            EkstepEditor.jQuery('#searchTextBox').bind("enterKey", function(e) {
+                searchText = EkstepEditor.jQuery('#searchTextBox').val();
+                (searchText === "") ? searchText = undefined: null;
+                instance.getImageFromServer(searchText);
+            });
+
+            EkstepEditor.jQuery('#searchButton').click(function(e) {
+                searchText = EkstepEditor.jQuery('#searchTextBox').val();
+                (searchText === "") ? searchText = undefined: null;
+                instance.getImageFromServer(searchText);
+            });
+
+
+            EkstepEditor.jQuery('#searchTextBox').keyup(function(e) {
+                if (e.keyCode == 13) {
+                    EkstepEditor.jQuery(this).trigger("enterKey");
+                }
             });
 
             EkstepEditor.jQuery('#selectanduse').addClass('disabled');
@@ -131,15 +179,15 @@ EkstepEditor.basePlugin.extend({
                 strVar += "    <text style=\"font-size: 20px;\">Add Image<\/text>";
                 strVar += "    <uib-tabset active=\"active\">";
                 strVar += "        <uib-tab index=\"0\" heading=\"Image\">";
-                strVar += "            <input type=\"text\" style=\"width: 90%; float: left;\" placeholder=\"search image...\" \/>";
+                strVar += "            <input id=\"searchTextBox\" type=\"text\" style=\"width: 95%; float: left; font-size: 20px;\" placeholder=\"search image...\" \/>";
                 strVar += "            <span>";
-                strVar += "                    <button class=\"btn btn-primary\"><i class=\"fa fa-search\" aria-hidden=\"true\"><\/i><\/button>";
+                strVar += "                    <button id=\"searchButton\" class=\"btn btn-primary\"><i class=\"fa fa-search\" aria-hidden=\"true\"><\/i><\/button>";
                 strVar += "                    <\/span>";
-                strVar += "            <div class=\"col-xs-8 col-sm-8 col-md-8 col-lg-8 scrollWindow\">";
-                strVar += "                <div id=\"assestArea\" class=\"row\">";
+                strVar += "            <div id=\"assetWindow\" class=\"col-xs-8 col-sm-8 col-md-8 col-lg-8 scrollWindow\">";
+                strVar += "                <div id=\"assetArea\" class=\"row\">";
                 strVar += "                <\/div>";
                 strVar += "            <\/div>";
-                strVar += "            <div class=\"col-xs-4 col-sm-4 col-md-4 col-lg-4\" style=\"padding: 25px; font-size: 25px;\"> Select an asset to view properties";
+                strVar += "            <div class=\"col-xs-4 col-sm-4 col-md-4 col-lg-4\" style=\"padding: 25px; font-size: 25px;\">";
                 strVar += "            <\/div>";
                 strVar += "        <\/uib-tab>";
                 strVar += "    <\/uib-tabset>";
@@ -151,7 +199,7 @@ EkstepEditor.basePlugin.extend({
 
             modal_footer: function() {
                 var strVar = "";
-                strVar += "<button id=\"selectanduse\" class=\"btn btn-primary\" type=\"button\">Select and Use<\/button>";
+                strVar += "<button id=\"selectanduse\" class=\"btn btn-primary\" type=\"button\">Select<\/button>";
                 strVar += "<button id=\"closePopUp\" class=\"btn btn-warning\" type=\"button\">Cancel<\/button>";
                 return strVar;
             }
