b8d6404bf3302b241f7e41c9d4c6c65a8c876dbe
sunil <sunils@ilimi.in>
2016-11-21 12:25:02 +0530

    fix: image plugin
    changes:
    - media tag not populating src
    
    contributes: #10

--- org.ekstep.image-1.0/editor/plugin.js
+++ org.ekstep.image-1.0/editor/plugin.js
@@ -2,6 +2,8 @@ EkstepEditor.basePlugin.extend({
     initialize: function() {
         var instance = this;
         EkstepEditorAPI.addEventListener("imagebrowser:add", function(event, data) {
+            data.media = {};
+            data.media[data.asset] = { src: data.src };
             EkstepEditorAPI.dispatchEvent(instance.manifest.id + ':create', data);
         }, this);
     },
@@ -10,6 +12,7 @@ EkstepEditor.basePlugin.extend({
         var _parent = this.parent;
         this.parent = undefined;
         var props = this.convertToFabric(this.attributes);
+        this.media = this.attributes.media;
         var media = this.media[this.attributes.asset];
         var imageURL = EkstepEditorAPI.globalContext.useProxyForURL ? "image/get/" + encodeURIComponent(media.src) : media.src;
         fabric.Image.fromURL(imageURL, function(img) {
