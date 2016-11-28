292f3c11e2b1bd8a90f2560a5bf1a1239e91fb28
Harishkumar Gangula <harishg@ilimi.in>
2016-11-28 22:08:01 +0530

    feat: Docs for colorpicker - Colorpicker Plugin Documentations added contributes:https://github.com/ekstep/Content-Plugins/issues/9

--- org.ekstep.colorpicker-1.0/editor/plugin.js
+++ org.ekstep.colorpicker-1.0/editor/plugin.js
@@ -1,14 +1,47 @@
+/**
+ * The purpose of {@link ColorPicker} is used to add color to other plugin objects
+ *
+ * @class ColorPicker
+ * @extends EkstepEditor.basePlugin
+ *
+ * @author Harishkumar Gangula <harishg@ilimi.in>
+ */
 EkstepEditor.basePlugin.extend({
+    /**
+     * This expains the type of the plugin 
+     * @member {String} type
+     * @memberof ColorPicker
+     */
     type: "colorpicker",
+    /**
+     * This contains the life cycle  methods of colorpicker
+     * @member {undefined|Object} picker
+     * @memberof ColorPicker
+     */
     picker: undefined,
+    /**
+     * The events are registred which are used to show, update and initialize the colorpicker
+     * @memberof ColorPicker
+     */
     initialize: function() {
         EkstepEditorAPI.addEventListener("colorpicker:show", this.showColorPicker, this);
         EkstepEditorAPI.addEventListener("colorpicker:state", this.invoke, this);
         EkstepEditorAPI.addEventListener("colorpicker:update", this.updateColor, this);
     },
+    /**
+     * The method is used to show the color palette
+     * @memberof ColorPicker
+     */
     showColorPicker: function() {
         this.picker.show();
     },
+    /**
+     * The method is used to initiate the colorpicker
+     * it has the callback method which will be called on change of the color
+     * @param  {Object} event
+     * @param  {Object} data
+     * @memberof ColorPicker 
+     */
     invoke: function(event, data) {
         if (EkstepEditor.jQuery("#" + data.id).attr("colorpicker") != "added") {
             this.picker = new jscolor(document.getElementById(data.id), {
@@ -21,7 +54,7 @@ EkstepEditor.basePlugin.extend({
         }
         if (data && data.color) {
             this.picker.fromString(data.color);
-        } else{
+        } else {
             this.picker.fromString("#000000"); // default color will be black
         }
     }
