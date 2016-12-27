f0329ede873ea00c45066ebc43ed2875c7462c48
manjudr <manju143dr@gmail.com>
2016-12-27 11:29:56 +0530

    update: #64 making api call  to get the template data, adding template data to manifest

--- org.ekstep.assessment-1.0/editor/plugin.js
+++ org.ekstep.assessment-1.0/editor/plugin.js
@@ -23,9 +23,10 @@ EkstepEditor.basePlugin.extend({
     initialize: function() {
         EkstepEditorAPI.addEventListener(this.manifest.id + ":showPopup", this.openAssessmentBrowser, this);
     },
+    
     // Add assesment to the stage
     newInstance: function() {
-        var instance = this,attr = instance.attributes, ques = [],sets = {},ctrl = {};
+        var instance = this,attr = instance.attributes, ques = [],sets = {},ctrl = {},template ={};
         if (isNaN(attr.w)) {
             attr.w = attr.h = 70;
         }
@@ -33,34 +34,55 @@ EkstepEditor.basePlugin.extend({
             attr[i] = instance.cleanObject(attr[i]);
             ques.push(attr[i].question);
             instance.addMediatoManifest(attr[i].question.media);
+            // TODO : Refactor of getting template code
+            if (!_.isUndefined(attr[i].question.template_id)) {
+                EkstepEditor.assessmentService.getTemplate(attr[i].question.template_id, function(err, res) {                    
+                    template = instance.xmltojsonTempl(res);
+                    console.info("Template Data:", template);
+                    instance.addMediatoManifest(template.manifest.media);
+                });
+            }
         }
         instance.percentToPixel(attr);
-        var props = instance.convertToFabric(attr);
-        var count = attr[attr.length - 1].total_items;
-        var max_score = attr[attr.length - 1].max_score;
-        instance.editorObj = instance.editiorPage(props, count, max_score);
-        // TODO : Refactring of the objects
+        var props = instance.convertToFabric(attr),
+        itemArray = attr.length -1, 
+        count = attr[itemArray].total_items +"/" + itemArray,
+        max_score = attr[itemArray].max_score,
+        title = attr[itemArray].title;
+        instance.editorObj = instance.showProperties(props,title, count, max_score);
         sets[attr[0].question.identifier] = ques;
         ctrl["items"] = sets;
-        ctrl["item_sets"] = [{
-            "count": attr[attr.length - 1].total_items,
-            "id": attr[0].question.identifier
-        }]
-        instance.setData(Object.assign(ctrl, attr[attr.length - 1]));
-
+        ctrl["item_sets"] = [{"count": attr[itemArray].total_items,"id": attr[0].question.identifier}];
+        instance.setData(Object.assign(ctrl, attr[itemArray]));
         // TODO :setconfig should not be hard-coded
-        instance.setConfig({
-            "type": "items",
-            "var": "item"
-        });
+        instance.setConfig({"type": "items","var": "item"});
         delete instance.attributes;
+
+    },
+     // parsing of the items
+    cleanObject: function(item) {
+        if (!_.isUndefined(item.question.options)) {
+            item.question.options = !_.isObject(item.question.options) ? JSON.parse(item.question.options) : item.question.options;
+            return item;
+        } else if (!_.isUndefined(item.question.lhs_options) && !_.isUndefined(item.question.rhs_options)) {
+            item.question.lhs_options = !_.isObject(item.question.lhs_options) ? JSON.parse(item.question.lhs_options) : item.question.lhs_options;
+            item.question.rhs_options = !_.isObject(item.question.rhs_options) ? JSON.parse(item.question.rhs_options) : item.question.rhs_options;
+            return item;
+        } else if (!_.isUndefined(item.question.model) && !_.isUndefined(item.question.answer)) {
+            item.question.model = !_.isObject(item.question.model) ? JSON.parse(item.question.model) : item.question.model;
+            item.question.answer = !_.isObject(item.question.answer) ? JSON.parse(item.question.answer) : item.question.answer;
+            return item;
+        } else {
+            console.warn("Selected item is not valid",item);
+            return item;
+        }
 
     },
     // get media asset and add those asset to the manifest
     addMediatoManifest: function(arryAttr) {
         var asset, instance = this;
         if (!_.isUndefined(arryAttr)) {
-            asset = JSON.parse(arryAttr);
+           asset = !_.isObject(arryAttr) ? JSON.parse(arryAttr) : arryAttr;
             if (asset.length > 0) {
                 asset.forEach(function(ele, index) {
                     if (!_.isNull(asset[index].id)) {
@@ -70,31 +92,23 @@ EkstepEditor.basePlugin.extend({
             }
         }
     },
-    cleanObject: function(item) {
-        if (!_.isUndefined(item.question.options)) {
-            item.question.options = !_.isObject(item.question.options) ? JSON.parse(item.question.options) : item.question.options;
-            return item;
-        } else if (!_.isUndefined(item.question.lhs_options)) {
-            item.question.lhs_options = !_.isObject(item.question.lhs_options) ? JSON.parse(item.question.lhs_options) : item.question.lhs_options;
-            item.question.rhs_options = !_.isObject(item.question.rhs_options) ? JSON.parse(item.question.rhs_options) : item.question.rhs_options;
-            return item;
-        } else {
-            item.question.model = !_.isObject(item.question.model) ? JSON.parse(item.question.model) : item.question.model;
-            item.question.answer = !_.isObject(item.question.answer) ? JSON.parse(item.question.answer) : item.question.answer;
-            return item;
-        }
+    xmltojsonTempl : function(res){
+         var data, x2js = new X2JS({attributePrefix: 'none'});
+          if (!_.isNull(res)) {
+                    data = x2js.xml_str2json(res.data.result.content.body);
+                    return data.theme;
+                }
 
     },
-
     /*Display of Maxscore and question count on the editor page */
 
-    editiorPage: function(props, count, max_score) {
+    showProperties: function(props, title, count, max_score) {
         props.fill = "#EDC06D";
-        var rect = new fabric.Rect(props);
-        var qTittle = new fabric.Text("QUESTION SET", {fontSize: 30, fill:'black', textDecoration:'underline', top: 80, left: 150} );
-        var qCount = new fabric.Text("QUESTION COUNT : " + count, {fontSize: 20,fill:'black',top: 120,left: 150});
-        var max_score = new fabric.Text("MAX_SCORE : "+max_score, {fontSize: 20, fill:'black', top: 150,left: 150,});
-        var fabricGroup = new fabric.Group([rect, qTittle, qCount, max_score], {left: 110, top: 50});
+        var rect = new fabric.Rect(props),
+        qTittle = new fabric.Text(title, {fontSize: 30, fill:'black',textAlign:'center',textDecoration:'underline', top: 80, left: 150} ),
+        qCount = new fabric.Text("QUESTIONS : " + count, {fontSize: 20,fill:'black',top: 120,left: 150}),
+        max_score = new fabric.Text("TOTAL MARKS : "+max_score, {fontSize: 20, fill:'black', top: 150,left: 150,}),
+        fabricGroup = new fabric.Group([rect, qTittle, qCount, max_score], {left: 110, top: 50});
         return fabricGroup;
     },
     /**    
@@ -104,11 +118,11 @@ EkstepEditor.basePlugin.extend({
     * 
     */
     openAssessmentBrowser: function(event, callback) {
-        var instance = this,data = [];
+        var instance = this,set = [];
         var callback = function(items, config) {
-            data.push(items);
-            data[0].push(config);
-            EkstepEditorAPI.dispatchEvent(instance.manifest.id + ':create', data);
+            items.push(config);
+            set.push(items);
+            EkstepEditorAPI.dispatchEvent(instance.manifest.id + ':create', set);
         };
         EkstepEditorAPI.dispatchEvent("org.ekstep.assessmentbrowser:show", callback);
     }
