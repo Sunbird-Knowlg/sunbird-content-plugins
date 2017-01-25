38feb643e4b2bd35e416b648f77fe242d7720578
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-01-25 17:24:34 +0530

    Issue #72 fix: added the default value true to the optionShuffle

--- org.ekstep.quiz-1.0/editor/plugin.js
+++ org.ekstep.quiz-1.0/editor/plugin.js
@@ -281,6 +281,10 @@ EkstepEditor.basePlugin.extend({
                     this.config.showImmediateFeedback = value;
                     this.data.questionnaire.showImmediateFeedback = value;
                     break;
+                case 'optionShuffle':
+                    this.config.optionShuffle = value;
+                    this.data.questionnaire.optionShuffle = value;
+                    break;
             }
         }
         EkstepEditorAPI.render();
@@ -295,6 +299,7 @@ EkstepEditor.basePlugin.extend({
         config.showImmediateFeedback = this.data.questionnaire.showImmediateFeedback;
         config.max_score = this.data.questionnaire.max_score;
         config.title = this.data.questionnaire.title;
+        config.optionShuffle = _.isUndefined(this.data.questionnaire.optionShuffle) ? true : this.data.questionnaire.optionShuffle ;
         return config;
     },
     /**    
