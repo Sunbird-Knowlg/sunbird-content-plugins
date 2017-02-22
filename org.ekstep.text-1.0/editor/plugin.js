38ec7a7a5d577988d0b20d884d12bfce52713db1
sunil <sunils@ilimi.in>
2017-02-22 14:42:34 +0530

    Issue #218 fix: transparency for Text
    changes:
    - added new config param: colorRGBA, to capture alpha along with rgb

--- org.ekstep.text-1.0/editor/plugin.js
+++ org.ekstep.text-1.0/editor/plugin.js
@@ -41,6 +41,11 @@ EkstepEditor.basePlugin.extend({
      * @memberof Text
      */
     newInstance: function() {
+        if (this.attributes.colorRGBA) {
+            this.config.colorRGBA = this.attributes.colorRGBA;
+            delete this.attributes.colorRGBA;        
+        }
+
         var props = this.convertToFabric(this.attributes);
         delete props.__text;
         props.editable = false; // added to disable inline editing of exiting content
@@ -48,6 +53,7 @@ EkstepEditor.basePlugin.extend({
         if (this.attributes.__text == '') {
             textEditor.showEditor(this.id);
         }
+        if (this.config.colorRGBA) this.editorObj.setFill(this.config.colorRGBA);
     },
     /**
      * This method overridden from Ekstepeditor.basePlugin and here we double click event is added
@@ -134,6 +140,10 @@ EkstepEditor.basePlugin.extend({
                 this.editorObj.setFill(value);
                 this.attributes.color = value;
                 break;
+            case "colorRGBA":
+                this.editorObj.setFill(this.toRGBAFormat(value));                
+                this.config.colorRGBA = this.toRGBAFormat(value);
+                break;
         }
         EkstepEditorAPI.render();
         EkstepEditorAPI.dispatchEvent('object:modified', { target: EkstepEditorAPI.getEditorObject() });
@@ -145,6 +155,7 @@ EkstepEditor.basePlugin.extend({
     getConfig: function() {
         var config = this._super();
         config.color = this.attributes.color || this.attributes.fill;
+        config.colorRGBA = this.config.colorRGBA;
         config.fontfamily = this.attributes.fontFamily; 
         config.fontsize = this.attributes.fontSize;
         config.fontweight = this.attributes.fontweight || false;
@@ -226,6 +237,10 @@ EkstepEditor.basePlugin.extend({
             return c.propertyName === 'stroke';
         })
         return config;
+    },
+    toRGBAFormat: function(rgbaObject) {
+        if(typeof rgbaObject === "object") return "rgba("+rgbaObject.r+","+rgbaObject.g+","+rgbaObject.b+","+rgbaObject.a+")"
+        else return rgbaObject;
     }
 });
 //# sourceURL=textplugin.js
