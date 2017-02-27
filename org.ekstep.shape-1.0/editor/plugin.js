ad6766078f9899c282c585f80023c4cbd9c392d1
sunil <sunils@ilimi.in>
2017-02-27 18:43:14 +0530

    Issue #218 revert: transparency for shapes
    contributes: #73

--- org.ekstep.shape-1.0/editor/plugin.js
+++ org.ekstep.shape-1.0/editor/plugin.js
@@ -18,11 +18,6 @@ EkstepEditor.basePlugin.extend({
      *
      */
     newInstance: function() {
-        if (this.attributes.colorRGBA) {
-            this.config.colorRGBA = this.attributes.colorRGBA;
-            delete this.attributes.colorRGBA;        
-        }
-
         var props = this.convertToFabric(this.attributes);
         switch (this.attributes.type) {
             case 'ellipse':
@@ -106,10 +101,7 @@ EkstepEditor.basePlugin.extend({
 
             default:
         }
-        if (this.editorObj) {
-            //if (props.fill) this.editorObj.setFill(props.fill);
-            if (this.config.colorRGBA) this.editorObj.setFill(this.config.colorRGBA);
-        }
+        if (this.editorObj) this.editorObj.setFill(props.fill);
     },
     /**
      *
@@ -123,10 +115,6 @@ EkstepEditor.basePlugin.extend({
             case 'color':
                 this.editorObj.setFill(value);
                 this.attributes.fill = value;
-                break;
-            case 'colorRGBA':                
-                this.editorObj.setFill(this.toRGBAFormat(value));                
-                this.config.colorRGBA = this.toRGBAFormat(value);
                 break;
             case 'radius':
                 if (this.attributes.type == 'ellipse') {
@@ -184,8 +172,7 @@ EkstepEditor.basePlugin.extend({
      */
     getConfig: function() {
         var config = this._super();
-        config.color = this.attributes.fill;       
-        config.colorRGBA = this.config.colorRGBA || config.color;
+        config.color = this.attributes.fill;               
         if (this.attributes.type == 'roundrect') {
             config.radius = this.editorObj.rx;
         }
@@ -231,10 +218,6 @@ EkstepEditor.basePlugin.extend({
 
         // Other convex polygons
         "trapezium" : [{"x":25,"y":0},{"x":75,"y":0},{"x":100,"y":100},{"x":0,"y":100}]
-    },
-    toRGBAFormat: function(rgbaObject) {
-        if(typeof rgbaObject === "object") return "rgba("+rgbaObject.r+","+rgbaObject.g+","+rgbaObject.b+","+rgbaObject.a+")"
-        else return rgbaObject;
     }
 });
 //# sourceURL=shapeplugin.js
