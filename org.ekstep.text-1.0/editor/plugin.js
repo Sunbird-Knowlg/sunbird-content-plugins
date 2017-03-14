dc347dd2ffe8a24719c14cb2aa9a9c0f6dd44ffd
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-03-14 15:27:30 +0530

    Issue #304 fix: for text default empty string assigning

--- org.ekstep.text-1.0/editor/plugin.js
+++ org.ekstep.text-1.0/editor/plugin.js
@@ -48,6 +48,9 @@ EkstepEditor.basePlugin.extend({
     newInstance: function() {
         var props = this.convertToFabric(this.attributes);
         delete props.__text;
+        if(EkstepEditorAPI._.isUndefined(this.attributes.__text)){
+            this.attributes.__text = '';
+        }
         props.editable = false; // added to disable inline editing of exiting content
         this.editorObj = new fabric.ITextbox(this.attributes.__text, props);
         if (this.attributes.__text == '') {
