85b6a9f4780d081f0449e92945039aadb7d907bd
Santhosh <santhosh@ilimi.in>
2017-04-02 13:19:38 +0530

    Issue ekstep/Content-Editor#268 fix: Capability in stage for refresh thumbnail

--- org.ekstep.stage-1.0/editor/plugin.js
+++ org.ekstep.stage-1.0/editor/plugin.js
@@ -128,6 +128,12 @@ EkstepEditor.basePlugin.extend({
                 }
                 break;
         }
+    },
+    updateThumbnail: function() {
+        $('<canvas>').attr({ id: this.id }).css({ width: '720px', height: '405px' }).appendTo('#thumbnailCanvasContainer');
+        canvas = new fabric.Canvas(this.id, { backgroundColor: "#FFFFFF", preserveObjectStacking: true, width: 720, height: 405 });
+        this.render(canvas);
+        ecEditor.jQuery('#' + this.id).remove();
     }
 });
 //# sourceURL=stageplugin.js
