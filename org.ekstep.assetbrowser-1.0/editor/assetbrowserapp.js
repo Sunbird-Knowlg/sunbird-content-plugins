f7e8eb68ab51808396b678a87c9fdfed6b2fc79b
sunil <sunils@ilimi.in>
2017-07-20 13:17:38 +0530

    Issue #366 fix: asset browser lames url issue

--- org.ekstep.assetbrowser-1.0/editor/assetbrowserapp.js
+++ org.ekstep.assetbrowser-1.0/editor/assetbrowserapp.js
@@ -2,7 +2,7 @@
 angular.module('assetbrowserapp', ['angularAudioRecorder']).config(['recorderServiceProvider', function(recorderServiceProvider) {
 
     recorderServiceProvider.forceSwf(false);
-    var lameJsUrl = ecEditor.resolvePluginResource("org.ekstep.assetbrowser", "1.0", "editor/recorder/lib2/lame.min.js");
+    var lameJsUrl = window.location.origin + ecEditor.resolvePluginResource("org.ekstep.assetbrowser", "1.0", "editor/recorder/lib2/lame.min.js");
     var config = {
         lameJsUrl: lameJsUrl,
         bitRate: 92
