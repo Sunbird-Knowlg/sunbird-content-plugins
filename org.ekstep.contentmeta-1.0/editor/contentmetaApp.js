cd44bc568084399a38c0680fdcfcfdf5331d5ba1
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-24 16:14:34 +0530

    Issue #478 update: collection editor meta forms

--- org.ekstep.contentmeta-1.0/editor/contentmetaApp.js
+++ org.ekstep.contentmeta-1.0/editor/contentmetaApp.js
@@ -31,6 +31,9 @@ angular.module('contentmetaApp', []).controller('contentmetaController', ['$scop
             if(_.isString($scope.content.keywords)){
                 $scope.content.keywords = $scope.content.keywords.split(',');
             }
+            if(_.isString($scope.textbook.language)){
+                $scope.textbook.language = [$scope.textbook.language];
+            }
             $scope.content.contentType = $scope.nodeType;
             org.ekstep.collectioneditor.api.getService('collection').setNodeTitle($scope.content.name);
             org.ekstep.collectioneditor.cache.nodesModified[$scope.nodeId].metadata = _.assign(org.ekstep.collectioneditor.cache.nodesModified[$scope.nodeId].metadata , $scope.getUpdatedMetadata($scope.metadataCloneObj, $scope.content));;
