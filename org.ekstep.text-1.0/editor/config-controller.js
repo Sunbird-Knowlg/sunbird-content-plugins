62248186b661ce8e6ad562b866f0b8eb306016b5
Anshu Mishra <anshu.mishra@goodworklabs.com>
2017-07-26 19:43:13 +0530

    Issue #369 style: UI changes for text plugin

--- org.ekstep.text-1.0/editor/config-controller.js
+++ org.ekstep.text-1.0/editor/config-controller.js
@@ -1,6 +1,11 @@
 angular.module('editorApp')
-.controller('org.ekstep.text:config-controller', ['$scope', function($scope) {
-    $scope.openTransliterator = function() {
-    	ecEditor.dispatchEvent("org.ekstep.text:showpopup");
-    };
-}]);
+    .controller('org.ekstep.text:config-controller', ['$scope', function($scope) {
+        $scope.openTransliterator = function() {
+            ecEditor.dispatchEvent("org.ekstep.text:showpopup");
+        };
+
+        ecEditor.jQuery('.ui.accordion').accordion();
+        ecEditor.jQuery('.font-face-dropdown').dropdown({
+        	'set selected': '1'
+        });
+    }]);
