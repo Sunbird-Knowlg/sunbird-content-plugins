c149090c7cb61d7f9bc332abb2ec51d1cb27cce9
Anshu Mishra <anshu.mishra@goodworklabs.com>
2017-07-27 19:16:13 +0530

    Issue #530 style:Accordion behaviour implemented in text-plugin

--- org.ekstep.config-1.0/editor/colorpicker-directive.js
+++ org.ekstep.config-1.0/editor/colorpicker-directive.js
@@ -2,7 +2,7 @@ angular.module('editorApp').directive('uiColorpicker', function() {
     return {
         restrict: 'E',
         replace: true,
-        template: '<span><input class="input" id="{{config.propertyName}}" id="{{config.propertyName}}" ng-click="fireSidebarTelemetry({id: config.propertyName}, config.dataType)"/></span>',
+        template: '<span><input class="input" id="{{config.propertyName || \'color\'}}" id="{{config.propertyName || \'color\'}}" ng-click="fireSidebarTelemetry({id: config.propertyName || \'color\'}, config.dataType)"/></span>',
         link: function(scope, element) {
             var input = element.find('input');
             input.spectrum({
