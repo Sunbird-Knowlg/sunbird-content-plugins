45c40fc0c7ab221d26f760c6b86027c2dbcca114
Anshu Mishra <anshu.mishra@goodworklabs.com>
2017-08-29 15:06:35 +0530

    Issue #586 style: add popup to icons in header

--- org.ekstep.genericeditorheader-1.0/editor/headerApp.js
+++ org.ekstep.genericeditorheader-1.0/editor/headerApp.js
@@ -2,4 +2,8 @@ angular.module('org.ekstep.genericeditor', ["Scope.safeApply", "yaru22.angular-t
    var plugin = { id: "org.ekstep.genericeditorheader", ver: "1.0" };
    $scope.ekstepLogo = ecEditor.resolvePluginResource(plugin.id, plugin.ver, "editor/images/ekstep_logo_white.png");
 
+   setTimeout(function(){
+   		ecEditor.jQuery('.popup-item').popup();
+   },10);
+
 }]);
