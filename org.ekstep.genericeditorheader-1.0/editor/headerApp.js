6058a7b227c8c0ee7f927c229789eb866d2f4a53
ruchikanagarkar <ruchikanagarkar@outlook.com>
2017-08-29 16:46:56 +0530

    Issue #588 feat: download message modified

--- org.ekstep.genericeditorheader-1.0/editor/headerApp.js
+++ org.ekstep.genericeditorheader-1.0/editor/headerApp.js
@@ -13,22 +13,22 @@ angular.module('org.ekstep.genericeditor', ["Scope.safeApply", "yaru22.angular-t
     }
 
    $scope.download = function(){
-   		var fileName = (ecEditor.getService('content').getContentMeta(ecEditor.getContext('contentId')).name).toLowerCase();
+      var fileName = (ecEditor.getService('content').getContentMeta(ecEditor.getContext('contentId')).name).toLowerCase();
         ecEditor.getService('content').downloadContent(ecEditor.getContext('contentId'), fileName, function(err, resp) {
             if (!err && resp.data.responseCode == "OK") {
-				var link = document.createElement('a');
-				link.href = resp.data.result.ECAR_URL;
-				link.download = link.href;
-				link.style.display = 'none';
-				document.body.appendChild(link);
-			    link.click();
-			    document.body.removeChild(link);  
-			    
-			    ecEditor.dispatchEvent("org.ekstep.toaster:success", {
-                    title: 'Content downloaded successfully!',                    
-                    position: 'topCenter',
-                    icon: 'fa fa-check-circle'
-                });     
+              var link = document.createElement('a');
+              link.href = resp.data.result.ECAR_URL;
+              link.download = link.href;
+              link.style.display = 'none';
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);  
+              
+              ecEditor.dispatchEvent("org.ekstep.toaster:success", {
+                        title: 'Content download started!',                    
+                        position: 'topCenter',
+                        icon: 'fa fa-check-circle'
+                    });     
             } else {
                 ecEditor.dispatchEvent("org.ekstep.toaster:error", {
                     message: 'Unable to download the content, please try again later',
