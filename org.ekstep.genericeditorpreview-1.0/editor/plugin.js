0d8de57b67b8ddebbddf50452baa1bdcfd6ce75c
manjunath davanam <manjunathd@ilimi.in>
2017-08-29 12:35:42 +0530

    Issue #586 feat: generic editor preview plugin

--- org.ekstep.genericeditorpreview-1.0/editor/plugin.js
+++ org.ekstep.genericeditorpreview-1.0/editor/plugin.js
@@ -2,8 +2,8 @@
  * 
  * plugin for preview stage contents
  * @class Preview
- * @extends org.ekstep.contenteditor.basePlugin
- * @author Sunil A S <sunils@ilimi.in>
+ * @extends org.ekstep.genericeditor.basePlugin
+ * @author Manjunath Davanam <manjunathd@ilimi.in>
  * @listens atpreview:show
  */
 org.ekstep.genericeditor.basePlugin.extend({
@@ -18,7 +18,7 @@ org.ekstep.genericeditor.basePlugin.extend({
      *   @memberof Preview
      *
      */
-    previewURL: (ecEditor.getConfig('previewURL') || '/content/preview/preview.html') + '?webview=true',
+    previewURL: 'preview/preview.html' + '?webview=true',
     /**
      *   @member contentBody {Object} content body for preview
      *   @memberof Preview
@@ -33,7 +33,6 @@ org.ekstep.genericeditor.basePlugin.extend({
     initialize: function() {
         ecEditor.addEventListener("atpreview:show", this.initPreview, this);
         var templatePath = ecEditor.resolvePluginResource(this.manifest.id, this.manifest.ver, "editor/contentPreview.html");
-        //ecEditor.getService('popup').loadNgModules(templatePath);
     },
     /**
      *
@@ -42,45 +41,44 @@ org.ekstep.genericeditor.basePlugin.extend({
      *   @memberof preview
      */
     initPreview: function(event, data) {
-        this.contentBody = data.contentBody;
-        if(data.currentStage){
-            this.contentBody.theme.startStage = ecEditor.getCurrentStage().id;
-        }
-        var scope = ecEditor.getAngularScope();
-        if (scope.developerMode) {
-            if(!this.contentBody.theme['plugin-manifest']) this.contentBody.theme['plugin-manifest'] = {"plugin": []};
-            if(!_.isArray(this.contentBody.theme['plugin-manifest'].plugin)) this.contentBody.theme['plugin-manifest'].plugin = [this.contentBody.theme['plugin-manifest'].plugin];
-            this.contentBody.theme['plugin-manifest'].plugin.splice(0, 0, {
-                "id": "org.ekstep.developer",
-                "ver": "1.0",
-                "type": "plugin",
-                "hostPath": org.ekstep.pluginframework.hostRepo.basePath,
-                "preload": true
-            });
-        }
+        //this.contentBody = data.contentBody;
         this.showPreview();
     },
     /**     
      *   @memberof preview
      */
-    showPreview: function() {        
+    showPreview: function() {
         console.log(this.previewURL);
         var instance = this;
         var contentService = ecEditor.getService('content');
         var meta = ecEditor.getService('content').getContentMeta(ecEditor.getContext('contentId'));
-        var modalController = function($scope) {
-                var previewContentIframe = ecEditor.jQuery('#previewContentIframe')[0];
-                previewContentIframe.src = instance.previewURL;
-                var userData = ecEditor.getService('telemetry').context;
-                previewContentIframe.onload = function() {
-                    var configuration = {};
-                    userData.etags = userData.etags || {};
-                    configuration.context = {'mode':'edit','contentId':meta.identifier,'sid':userData.sid,'uid':userData.uid, 'channel': userData.channel, 'pdata': userData.pdata, 'app': userData.etags.app, 'dims': userData.etags.dims, 'partner': userData.etags.partner }; 
-                    configuration.config = {'showEndPage':'true','showStartPage':'true'};
-                    configuration.metadata = meta.contentMeta; configuration.data = instance.contentBody;
-                    previewContentIframe.contentWindow.initializePreview(configuration);
-                    
-                };
+        var previewContentIframe = ecEditor.jQuery('#previewContentIframe')[0];
+        previewContentIframe.src = instance.previewURL;
+        var userData = ecEditor.getService('telemetry').context;
+        previewContentIframe.onload = function() {
+            var configuration = {};
+            userData.etags = userData.etags || {};
+            configuration.context = {
+                'mode': 'edit',
+                'contentId': meta.identifier,
+                'sid': userData.sid,
+                'uid': userData.uid,
+                'channel': userData.channel,
+                'pdata': userData.pdata,
+                'app': userData.etags.app,
+                'dims': userData.etags.dims,
+                'partner': userData.etags.partner,
+                'contentId':'do_1123102903042129921225'
+            };
+            configuration.config = {
+                'showEndPage': 'true',
+                'showStartPage': 'true'
+            };
+            configuration.data = {};
+            configuration.metadata = meta.contentMeta;
+            configuration.data = instance.contentBody;
+            previewContentIframe.contentWindow.initializePreview(configuration);
+
         };
     }
 });
