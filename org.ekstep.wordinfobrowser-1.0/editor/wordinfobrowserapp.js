4005d4e15b27b41ce93c4f42462d39592f4a883e
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-10-04 18:44:17 +0530

    Issue #203 fix: reverting wordinfo to older version

--- org.ekstep.wordinfobrowser-1.0/editor/wordinfobrowserapp.js
+++ org.ekstep.wordinfobrowser-1.0/editor/wordinfobrowserapp.js
@@ -11,35 +11,17 @@ angular.module('wordinfobrowserapp', []).controller('wordinfobrowsercontroller',
     ctrl.loading = false;
     ctrl.noKeywords = false;
     ctrl.errorLoadingKeywords = false;
-    ctrl.template = {
-        "id": "infoTemplate",
-        "g": {
-            "x": "0",
-            "y": "0",
-            "w": "100",
-            "h": "100",
-            "text": [{ "x": "48", "y": "15", "w": "50", "h": "9", "visible": "true", "model": "word.lemma", "weight": "normal", "font": "bold", "color": "rgb(0,0,0)", "fontsize": "85", "align": "left", "z-index": "2", "id": "lemma" },
-                { "x": "48", "y": "25", "w": "40", "h": "30", "visible": "true", "model": "word.gloss", "weight": "100", "font": "helvetica", "color": "rgb(0,0,0)", "fontsize": "55", "align": "left", "z-index": "2", "id": "gloss" },
-                { "x": "48", "y": "50", "w": "40", "h": "50", "visible": "true", "model": "word.exampleSentences", "weight": "100", "font": "helvetica", "color": "rgb(0,0,0)", "fontsize": "55", "align": "left", "z-index": "2", "id": "exampleSentences" }],
-            "shape": [{ "x": "10", "y": "10", "w": "80", "h": "80", "visible": "true", "editable": "true", "type": "roundrect", "radius": "10", "stroke-width": "1", "fill": "#ffffff", "id": "textBg", "z-index": "1" },
-                {
-                    "x": "10",
-                    "y": "10",
-                    "w": "35",
-                    "h": "55",
-                    "z-index": "2",
-                    "stroke": "#4c4c4c",
-                    "type": "roundrect",
-                    "radius": "10",
-                    "stroke-width": "2"
-                },
-                { "x": "10", "y": "10", "w": "80", "h": "80", "visible": "true", "editable": "true", "type": "roundrect", "radius": "10", "stroke-width": "1", "z-index": "1", "stroke": "#e1e4e8", "stroke-width": "3" }
-            ],
-            "image": [{ "asset": "org.ekstep.wordinfobrowser.popuptint", "x": "0", "y": "0", "w": "100", "h": "100", "visible": "true", "id": "popup-Tint" },
-                { "model": "word.picture", "x": "12", "y": "14", "w": "30", "h": "50", "stretch": "false", "visible": "true", "id": "imageid", "z-index": "2" }
-            ]
-        }
-    };
+    ctrl.template = {"id": "infoTemplate",
+                    "g": {
+                        "x": "0",
+                        "y": "0",
+                        "w": "100",
+                        "h": "100",
+                        "image": { "asset": "org.ekstep.text.popuptint", "x": "0", "y": "0", "w": "100", "h": "100", "visible": "true", "id": "popup-Tint" },
+                        "text": [{ "x": "25", "y": "25", "w": "50", "h": "9", "visible": "true", "editable": "true", "model": "word.lemma", "weight": "normal", "font": "helvetica", "color": "rgb(0,0,0)", "fontsize": "75", "align": "left", "z-index": "1", "id": "lemma" }, { "x": "25", "y": "35", "w": "50", "h": "40", "visible": "true", "editable": "true", "model": "word.gloss", "weight": "normal", "font": "helvetica", "color": "rgb(0,0,0)", "fontsize": "43", "align": "left", "z-index": "2", "id": "gloss" }],
+                        "shape": { "x": "20", "y": "20", "w": "60", "h": "60", "visible": "true", "editable": "true", "type": "roundrect", "radius": "10", "opacity": "1", "fill": "#45b3a5", "stroke-width": "1", "z-index": "0", "id": "textBg" }
+                        }
+                    };
 
     ctrl.selectWords = function($index, word, $event) {
         if (ecEditor._.indexOf(ctrl.slectedWords, word) != -1) {
@@ -104,15 +86,10 @@ angular.module('wordinfobrowserapp', []).controller('wordinfobrowsercontroller',
                 var dictionary = {};
                 ecEditor._.forEach(response.data.result.words, function(w) {
                     dictionary[w.lemma] = {
-                        lemma: w.lemma.toUpperCase(),
-                        picture: _.isArray(w.pictures) ? ctrl.generateId(w.pictures[0]) : "org.ekstep.wordinfobrowser.image",
-                        picturepath: _.isArray(w.pictures) ? w.pictures[0] : ecEditor.resolvePluginResource(instance.manifest.id, instance.manifest.ver, "assets/no-image.png"),
-                        exampleSentences: _.isArray(w.exampleSentences) ?"Example: "+ w.exampleSentences[0] : "",
+                        lemma: w.lemma,
                         gloss: ecEditor._.isUndefined(w.meaning) ? "" : w.meaning
                     }
-
                 });
-                ctrl.addAllMedia(dictionary);
                 var configData = {
                     "controller": {
                         "id" : "dictionary",
@@ -132,39 +109,7 @@ angular.module('wordinfobrowserapp', []).controller('wordinfobrowsercontroller',
             }
         });
     }
-    ctrl.generateId = function(value) {
-        var tempValue = value.split("/");
-        return tempValue[tempValue.length - 1].split(".").join().replace(",", '') + "_Id";
 
-    }
-    ctrl.addAllMedia = function(words) {
-        var wordsData = ecEditor._.clone(words);
-        var mediaAssets = [{
-                id: "org.ekstep.wordinfobrowser.popuptint",
-                src: ecEditor.resolvePluginResource(instance.manifest.id, instance.manifest.ver, 'assets/popuptint.png'),
-                assetId: "org.ekstep.wordinfobrowser.popuptint",
-                type: "image",
-                preload: true
-
-            }];
-        var picture = {};
-        _.each(wordsData, function(word) {
-            if (word.picture != undefined) {
-                picture = {
-                    id: word.picture,
-                    assetId: word.picture,
-                    src: word.picturepath,
-                    type: "image"
-                };
-                mediaAssets.push(picture);
-            }
-        });
-        var textObj = ecEditor.getCurrentObject();
-        var ins = ecEditor.getPluginInstance(textObj.id);
-        ecEditor._.forEach(mediaAssets, function(asset) {
-            ins.addMedia(asset);
-        });
-    }
     ctrl.showTextbox = function() {
         ctrl.showTypeownText = true;
         ctrl.noKeywords = false;
