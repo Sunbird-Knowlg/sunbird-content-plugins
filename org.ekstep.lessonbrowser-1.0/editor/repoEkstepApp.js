cffbe9039b875ce99a1473ea2d0bce68dd03a829
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-11-08 10:42:59 +0530

    Issue #706 fix: lesson browser search support channel specific changes in version 1.0

--- org.ekstep.lessonbrowser-1.0/editor/repoEkstepApp.js
+++ org.ekstep.lessonbrowser-1.0/editor/repoEkstepApp.js
@@ -105,7 +105,8 @@ angular.module('org.ekstep.contentprovider', [])
             }
             $scope.$safeApply();
         });
-        
+        // Reset the lesson selection on re-search
+        $scope.lessonSelection.splice(0, $scope.lessonSelection.length);
     };
 
     // Meta APIs integration
