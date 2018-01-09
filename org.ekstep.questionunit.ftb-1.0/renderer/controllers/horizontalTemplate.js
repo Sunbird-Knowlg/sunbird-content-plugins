548498a6d605adaa350f7c44253feca2a2abf214
“Gourav <gourav_m@tekditechnologies.com>
2018-01-09 12:34:28 +0530

    Issue #808 fix: Reindention

--- org.ekstep.questionunit.ftb-1.0/renderer/controllers/horizontalTemplate.js
+++ org.ekstep.questionunit.ftb-1.0/renderer/controllers/horizontalTemplate.js
@@ -1,66 +1,66 @@
 // TODO: Controller for horizontalTemplate.html
 'use strict';
 angular.module('FTBRendererApp', []).controller("FTBRendererController", function($scope) {
-    var ctrl = this;
-    $scope.questionObj = {
-        "question": "Choose the best",
-        "options": [{
-            "value": {
-            	"id": "a",
-                "type": "mixed",
-                "text": "This is an Apple This is an Apple This is an Apple This is an Apple",
-                "audio": "leg_aud",
-                "image": "home_img"
+  var ctrl = this;
+  $scope.questionObj = {
+    "question": "Choose the best",
+    "options": [{
+      "value": {
+       "id": "a",
+       "type": "mixed",
+       "text": "This is an Apple This is an Apple This is an Apple This is an Apple",
+       "audio": "leg_aud",
+       "image": "home_img"
 
-            }
-        }, {
-            "value": {
-            	"id": "b",
-                "type": "mixed",
-                "text": "Mango",
-                "audio": "earspell_aud",
-                "image": ""
-            }
-        }, {
-            "value": {
-            	"id": "c",
-                "type": "mixed",
-                "text": "Orange",
-                "audio": "earspell_aud",
-                "image": ""
-            }
-        }, {
-            "value": {
-            	"id": "d",
-                "type": "mixed",
-                "text": "Banana",
-                "audio": "earspell_aud",
-                "image": ""
+     }
+   }, {
+    "value": {
+     "id": "b",
+     "type": "mixed",
+     "text": "Mango",
+     "audio": "earspell_aud",
+     "image": ""
+   }
+ }, {
+  "value": {
+   "id": "c",
+   "type": "mixed",
+   "text": "Orange",
+   "audio": "earspell_aud",
+   "image": ""
+ }
+}, {
+  "value": {
+   "id": "d",
+   "type": "mixed",
+   "text": "Banana",
+   "audio": "earspell_aud",
+   "image": ""
 
-            },
+ },
 
-            "answer": true
-        }, {
-            "value": {
-            	"id": "e",
-                "type": "mixed",
-                "text": "Mango1",
-                "audio": "earspell_aud",
-                "image": ""
-            }
-        }, {
-            "value": {
-            	"id": "f",
-                "type": "mixed",
-                "text": "Orange1",
-                "audio": "earspell_aud",
-                "image": ""
-            }
-        },]
-    }
-    ctrl.selectedValue = function(value, event) {
-    	console.log(value,event.target.id);
-    	$("#"+event.target.id).parent().css("border","3px solid #166ca6").css("z-index","4").css("box-shadow","2px 2px 2px 2px #93bdda");
-    	$("#"+event.target.id).parent().siblings().css("border","2px solid #166ca6").css("z-index","1").css("box-shadow","none");
-    }
+ "answer": true
+}, {
+  "value": {
+   "id": "e",
+   "type": "mixed",
+   "text": "Mango1",
+   "audio": "earspell_aud",
+   "image": ""
+ }
+}, {
+  "value": {
+   "id": "f",
+   "type": "mixed",
+   "text": "Orange1",
+   "audio": "earspell_aud",
+   "image": ""
+ }
+},]
+}
+ctrl.selectedValue = function(value, event) {
+ console.log(value,event.target.id);
+ $("#"+event.target.id).parent().css("border","3px solid #166ca6").css("z-index","4").css("box-shadow","2px 2px 2px 2px #93bdda");
+ $("#"+event.target.id).parent().siblings().css("border","2px solid #166ca6").css("z-index","1").css("box-shadow","none");
+}
 });
