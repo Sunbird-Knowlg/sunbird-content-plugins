0e5bd5a94cd27d47ea698f0e9aaa67ad5aa275c2
Sachin <sachin.kumar@goodworklabs.com>
2018-01-09 13:16:28 +0530

    Issue #785 fix: merged with create-edit-question

--- org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
+++ org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
@@ -3,9 +3,9 @@
  * @class QuestionFormController
  * Swati singh <Swati.singh@tarento.com>
  */
- 'use strict';
- angular.module('createquestionapp', [])
- .controller('QuestionFormController', ['$scope', 'pluginInstance', function($scope, pluginInstance) {
+'use strict';
+angular.module('createquestionapp', [])
+  .controller('QuestionFormController', ['$scope', 'pluginInstance', function($scope, pluginInstance) {
 
     $scope.isQuestionTab = true;
     $scope.selectedQuestions = [];
@@ -19,204 +19,207 @@
     $scope.grades;
     $scope.languages = [];
     $scope.difficultyLevels = ['Easy', 'Medium', 'Difficult'];
-    $scope.questionTypes = ['mcq','ftb','mtf'];
-    $scope.filterObj={};
+    $scope.questionTypes = ['mcq', 'ftb', 'mtf'];
+    $scope.filterObj = {};
     $scope.pluginIdObj = {
-        "question_set_id": "org.ekstep.questionset",
-        "question_create_id": "org.ekstep.question",
-        "concepts_id": "org.ekstep.conceptselector"
+      "question_set_id": "org.ekstep.questionset",
+      "question_create_id": "org.ekstep.question",
+      "concepts_id": "org.ekstep.conceptselector"
     }
     $scope.csspath = ecEditor.resolvePluginResource(pluginInstance.manifest.id, pluginInstance.manifest.ver, 'editor/style.css');
 
     $scope.questionSetConfigObj = {
-        "title": "",
-        "max_score": 1,
-        "allow_skip": true,
-        "show_feedback": true,
-        "shuffle_questions": false,
-        "shuffle_options": false,
-        "total_items": 1
+      "title": "",
+      "max_score": 1,
+      "allow_skip": true,
+      "show_feedback": true,
+      "shuffle_questions": false,
+      "shuffle_options": false,
+      "total_items": 1
     };
 
-
+    /**
+     *  init funtion is called when html is loaded
+     *  @memberof QuestionFormController
+     */
     $scope.init = function() {
-        if (pluginInstance.editData) {
-            $scope.selectedQuestions = pluginInstance.editData.data;
-            $scope.questionSetConfigObj = pluginInstance.editData.config;
-            $scope.isQuestionTab = false;
-            $scope.isQuestionSetConfig = true;
-            $scope.createTotalItemRange();
-            for (var i = 0; i < $scope.selectedQuestions.length; i++) {
-                for (var j = 0; j < $scope.questions.length; j++) {
-                    if ($scope.selectedQuestions[i].questionId == $scope.questions[j].questionId) {
-                        $scope.questions[j].isSelected = true;
-                    }
-                }
+      if (pluginInstance.editData) {
+        $scope.selectedQuestions = pluginInstance.editData.data;
+        $scope.questionSetConfigObj = pluginInstance.editData.config;
+        $scope.isQuestionTab = false;
+        $scope.isQuestionSetConfig = true;
+        $scope.createTotalItemRange();
+        for (var i = 0; i < $scope.selectedQuestions.length; i++) {
+          for (var j = 0; j < $scope.questions.length; j++) {
+            if ($scope.selectedQuestions[i].questionId == $scope.questions[j].questionId) {
+              $scope.questions[j].isSelected = true;
             }
+          }
+        }
+      }
+
+      ecEditor.dispatchEvent($scope.pluginIdObj.concepts_id + ':init', {
+        element: 'queSetConceptsTextBox',
+        selectedConcepts: [], // All composite keys except mediaType
+        callback: function(data) {
+          $scope.Totalconcepts = data.length;
+          $scope.selectedConceptsData = data;
+          $scope.$safeApply();
+        }
+      });
+
+      ecEditor.getService('meta').getConfigOrdinals(function(err, res) {
+        if (!err) {
+          $scope.grades = res.data.result.ordinals.gradeLevel;
+          $scope.languages = res.data.result.ordinals.language;
+          $scope.$safeApply();
+        }
+        ecEditor.jQuery('.ui.dropdown.lableCls').dropdown({ useLabels: false, forceSelection: false });
+      });
+
+      ecEditor.addEventListener(pluginInstance.manifest.id + ":saveQuestion", function(event, data) {
+        data.isSelected = false;
+        var selQueIndex = _.findLastIndex($scope.questions, {
+          questionId: data.questionId
+        });
+        if (selQueIndex < 0) {
+          $scope.questions.push(data);
+        } else {
+          $scope.questions[selQueIndex] = data;
         }
 
-        ecEditor.dispatchEvent($scope.pluginIdObj.concepts_id + ':init', {
-            element: 'queSetConceptsTextBox',
-                selectedConcepts: [], // All composite keys except mediaType
-                callback: function(data) {
-                    $scope.Totalconcepts = data.length;
-                    $scope.selectedConceptsData = data;
-                    $scope.$safeApply();
-                }
-            });
 
-        ecEditor.getService('meta').getConfigOrdinals(function(err, res) {
-            if (!err) {
-                $scope.grades = res.data.result.ordinals.gradeLevel;
-                $scope.languages = res.data.result.ordinals.language;
-                $scope.$safeApply();
-            }
-            ecEditor.jQuery('.ui.dropdown.lableCls').dropdown({ useLabels: false, forceSelection: false});
-        });
-
-        ecEditor.addEventListener(pluginInstance.manifest.id + ":saveQuestion", function(event, data) {
-            data.isSelected = false;
-            var selQueIndex = _.findLastIndex($scope.questions, {
-                questionId: data.questionId
-            });
-            if (selQueIndex < 0) {
-                $scope.questions.push(data);
-            } else {
-                $scope.questions[selQueIndex] = data;
-            }
-
-
-        }, false);
+      }, false);
 
     }
 
-        /**
-         *  creating range of number of items to display as per number of question selected
-         *  @memberof QuestionFormController
-         */
-         $scope.createTotalItemRange = function() {
-            $scope.itemRange = [];
-            for (var i = 1; i <= $scope.selectedQuestions.length; i++) {
-                $scope.itemRange.push(i);
-            }
-        }
+    /**
+     *  creating range of number of items to display as per number of question selected
+     *  @memberof QuestionFormController
+     */
+    $scope.createTotalItemRange = function() {
+      $scope.itemRange = [];
+      for (var i = 1; i <= $scope.selectedQuestions.length; i++) {
+        $scope.itemRange.push(i);
+      }
+    }
 
-        /**
-         *  Creating list of selected questions for creating question set
-         *  @memberof QuestionFormController
-         */
-         $scope.selectQuestion = function(selQuestion) {
-            var selObjindex = $scope.selectedQuestions.indexOf(selQuestion);
-            if (selObjindex > -1) {
-                $scope.selectedQuestions.splice(selObjindex, 1);
-            }else{
-                $scope.selectedQuestions.push(selQuestion);
-            }
+    /**
+     *  Creating list of selected questions for creating question set
+     *  @memberof QuestionFormController
+     */
+    $scope.selectQuestion = function(selQuestion) {
+      var selObjindex = $scope.selectedQuestions.indexOf(selQuestion);
+      if (selObjindex > -1) {
+        $scope.selectedQuestions.splice(selObjindex, 1);
+      } else {
+        $scope.selectedQuestions.push(selQuestion);
+      }
 
-        }
+    }
 
-        /**
-         *  Funtion to edit the config data of question
-         *  @memberof QuestionFormController
-         */
-         $scope.editConfig = function(quesObj) {
-            $scope.selQuestionObj = angular.copy(quesObj);
-            $scope.showConfigForm = true;
-        }
-
-
-        /**
-         *  Funtion to remove question from selected question list
-         *  @memberof QuestionFormController
-         */
-         $scope.removeQuestion = function(selQuestion) {
-            var selObjindex = $scope.selectedQuestions.indexOf(selQuestion);
-            if (selObjindex > -1) {
-                $scope.selectedQuestions.splice(selObjindex, 1);
-            }
-            selObjindex = $scope.questions.indexOf(selQuestion);
-            if (selObjindex > -1) {
-                $scope.questions[selObjindex].isSelected = false;
-            }
-        }
+    /**
+     *  Funtion to edit the config data of question
+     *  @memberof QuestionFormController
+     */
+    $scope.editConfig = function(quesObj) {
+      $scope.selQuestionObj = angular.copy(quesObj);
+      $scope.showConfigForm = true;
+    }
 
 
-        /**
-         *  Funtion to remove question from selected question list
-         *  @memberof QuestionFormController
-         */
-         $scope.saveConfig = function() {
-            var selectedObjIndex = _.findLastIndex($scope.selectedQuestions, {
-                questionId: $scope.selQuestionObj.questionId
-            });
-            if (selectedObjIndex > -1) {
-                $scope.selectedQuestions[selectedObjIndex] = $scope.selQuestionObj;
-                $scope.showConfigForm = false;
-            }
-            selectedObjIndex = _.findLastIndex($scope.questions, {
-                questionId: $scope.selQuestionObj.questionId
-            });
-            if (selectedObjIndex > -1) {
-                $scope.questions[selectedObjIndex] = $scope.selQuestionObj;
-            }
-
-            delete $scope.questionObj;
-        }
+    /**
+     *  Funtion to remove question from selected question list
+     *  @memberof QuestionFormController
+     */
+    $scope.removeQuestion = function(selQuestion) {
+      var selObjindex = $scope.selectedQuestions.indexOf(selQuestion);
+      if (selObjindex > -1) {
+        $scope.selectedQuestions.splice(selObjindex, 1);
+      }
+      selObjindex = $scope.questions.indexOf(selQuestion);
+      if (selObjindex > -1) {
+        $scope.questions[selObjindex].isSelected = false;
+      }
+    }
 
 
+    /**
+     *  Funtion to remove question from selected question list
+     *  @memberof QuestionFormController
+     */
+    $scope.saveConfig = function() {
+      var selectedObjIndex = _.findLastIndex($scope.selectedQuestions, {
+        questionId: $scope.selQuestionObj.questionId
+      });
+      if (selectedObjIndex > -1) {
+        $scope.selectedQuestions[selectedObjIndex] = $scope.selQuestionObj;
+        $scope.showConfigForm = false;
+      }
+      selectedObjIndex = _.findLastIndex($scope.questions, {
+        questionId: $scope.selQuestionObj.questionId
+      });
+      if (selectedObjIndex > -1) {
+        $scope.questions[selectedObjIndex] = $scope.selQuestionObj;
+      }
 
-        /**
-         *  Funtion to save question set
-         *  @memberof QuestionFormController
-         */
-         $scope.createQuestionSet = function() {
-            $scope.questionSetConfigObj.total_items = $scope.selectedQuestions.length;
-            $scope.isQuestionSetConfig = true;
-            $scope.isQuestionTab = false;
-            $scope.createTotalItemRange();
-        }
-
-
-        /**
-         *  Funtion to add question set to editor. It dispatch an event to question set plugin for adding question set
-         *  @memberof QuestionFormController
-         */
-         $scope.addQuestionSet = function() {
-            var questionSet = {};
-            var callback = pluginInstance.callback;
-            questionSet.data = [];
-            questionSet.config = $scope.questionSetConfigObj;
-            questionSet.data = $scope.selectedQuestions;
-            ecEditor.dispatchEvent($scope.pluginIdObj.question_set_id + ":addQS", {callback: callback, data: questionSet});
-            $scope.closeThisDialog();
-        }
+      delete $scope.questionObj;
+    }
 
 
 
-        /**  Funtion to dispatch event to question creation plugin for creating new questions
-         *  @memberof QuestionFormController
-         */
-         $scope.createQuestion = function() {
-            ecEditor.dispatchEvent($scope.pluginIdObj.question_create_id + ":showpopup", {});
-        }
-
-        /**
-         * [createQuestion description]
-         * @return {[type]} [description]
-         */
-         $scope.editQuestion = function(questionObj) {
-            ecEditor.dispatchEvent($scope.pluginIdObj.question_create_id + ":showpopup", questionObj);
-        }
-
-        ctrl.previewItem = function() {};
-
-        $scope.cancel = function() {
-            $scope.closeThisDialog();
-        }
+    /**
+     *  Funtion to save question set
+     *  @memberof QuestionFormController
+     */
+    $scope.createQuestionSet = function() {
+      $scope.questionSetConfigObj.total_items = $scope.selectedQuestions.length;
+      $scope.isQuestionSetConfig = true;
+      $scope.isQuestionTab = false;
+      $scope.createTotalItemRange();
+    }
 
 
+    /**
+     *  Funtion to add question set to editor. It dispatch an event to question set plugin for adding question set
+     *  @memberof QuestionFormController
+     */
+    $scope.addQuestionSet = function() {
+      var questionSet = {};
+      var callback = pluginInstance.callback;
+      questionSet.data = [];
+      questionSet.config = $scope.questionSetConfigObj;
+      questionSet.data = $scope.selectedQuestions;
+      ecEditor.dispatchEvent($scope.pluginIdObj.question_set_id + ":addQS", { callback: callback, data: questionSet });
+      $scope.closeThisDialog();
+    }
 
-    }])
+
+
+    /**  Funtion to dispatch event to question creation plugin for creating new questions
+     *  @memberof QuestionFormController
+     */
+    $scope.createQuestion = function() {
+      ecEditor.dispatchEvent($scope.pluginIdObj.question_create_id + ":showpopup", {});
+    }
+
+    /**
+     * [createQuestion description]
+     * @return {[type]} [description]
+     */
+    $scope.editQuestion = function(questionObj) {
+      ecEditor.dispatchEvent($scope.pluginIdObj.question_create_id + ":showpopup", questionObj);
+    }
+
+    ctrl.previewItem = function() {};
+
+    $scope.cancel = function() {
+      $scope.closeThisDialog();
+    }
+
+
+
+  }])
 
 
 //# sourceURL=questionbankctrl.js
--- org.ekstep.questionset-1.0/editor/plugin.js
+++ org.ekstep.questionset-1.0/editor/plugin.js
@@ -5,68 +5,68 @@
  * @extends org.ekstep.contenteditor.basePlugin
  * @author Manoj Chandrashekar <manoj.chandrashekar@tarento.com>
  */
-org.ekstep.contenteditor.basePlugin.extend({
-    type: "org.ekstep.questionset",
-    _questions: [],
-    _questionPlugin: 'org.ekstep.question',
+ org.ekstep.contenteditor.basePlugin.extend({
+  type: "org.ekstep.questionset",
+  _questions: [],
+  _questionPlugin: 'org.ekstep.question',
     /**
      * Register events.
      * @memberof questionset
      */
-    initialize: function() {
-		var instance = this;
-        ecEditor.addEventListener(instance.manifest.id + ":showPopup", instance.openQuestionBank, instance);
-        ecEditor.addEventListener(instance.manifest.id + ":addQS", instance.addQS, instance);
+     initialize: function() {
+      var instance = this;
+      ecEditor.addEventListener(instance.manifest.id + ":showPopup", instance.openQuestionBank, instance);
+      ecEditor.addEventListener(instance.manifest.id + ":addQS", instance.addQS, instance);
     },
     newInstance: function() {
-       var instance = this;
-        delete this.configManifest;
-        var _parent = this.parent;
-        this.parent = undefined;
-        /*istanbul ignore else*/
-        if (!this.attributes.x) {
-            this.attributes.x = 10;
-            this.attributes.y = 10;
-            this.attributes.w = 80;
-            this.attributes.h = 80;
-            this.percentToPixel(this.attributes);
-        }
-        var props = this.convertToFabric(this.attributes);
-        delete props.width;
-        delete props.height;
+     var instance = this;
+     delete this.configManifest;
+     var _parent = this.parent;
+     this.parent = undefined;
+     /*istanbul ignore else*/
+     if (!this.attributes.x) {
+      this.attributes.x = 10;
+      this.attributes.y = 10;
+      this.attributes.w = 80;
+      this.attributes.h = 80;
+      this.percentToPixel(this.attributes);
+    }
+    var props = this.convertToFabric(this.attributes);
+    delete props.width;
+    delete props.height;
 
-        instance._questions = data.data ? data.data : [];
+    instance._questions = data.data ? data.data : [];
         // Add all question media to media manifest
         if (_.isArray(this._questions)) {
-            this._questions.forEach(function (question) {
-                if (_.isArray(question.media)) {
-                    question.media.forEach(function (mediaItem) {
-                        instance.addMedia(mediaItem);
-                    });
-                }
-            });
+          this._questions.forEach(function (question) {
+            if (_.isArray(question.media)) {
+              question.media.forEach(function (mediaItem) {
+                instance.addMedia(mediaItem);
+              });
+            }
+          });
         }
 
         // Add stage object
         var stageImage = ecEditor.resolvePluginResource(this.manifest.id, this.manifest.ver, 'editor/assets/QuizImage.png');
         instance.addMedia({
-            id: "QuizImage",
-            src: stageImage,
-            assetId: "QuizImage",
-            type: "image",
-            preload: true
+          id: "QuizImage",
+          src: stageImage,
+          assetId: "QuizImage",
+          type: "image",
+          preload: true
         });
         fabric.Image.fromURL(stageImage, function (img) {
-            var count = instance.config.total_items + '/' + instance._questions.length;
-            var quizDetails = instance.getPropsForEditor(instance.config.title, count, instance.config.max_score);
-            instance.editorObj = new fabric.Group([img, quizDetails]);
+          var count = instance.config.total_items + '/' + instance._questions.length;
+          var quizDetails = instance.getPropsForEditor(instance.config.title, count, instance.config.max_score);
+          instance.editorObj = new fabric.Group([img, quizDetails]);
             //instance.editorObj = img;
             instance.parent = _parent;
             instance.editorObj.scaleToWidth(props.w);
             instance.postInit();
-        }, props);
-    },
-    getPropsForEditor: function(qTittle, qCount, maxscore) {
+          }, props);
+      },
+      getPropsForEditor: function(qTittle, qCount, maxscore) {
         /* Display the all properties(title,count and maxscore) on the editor*/
         var instance = this;
         qTittle = new fabric.Text(qTittle.toUpperCase(), { fontSize: 15, fill: 'black', textAlign: 'center', top: 32, left: 105 });
@@ -74,54 +74,54 @@ org.ekstep.contenteditor.basePlugin.extend({
         maxscore = new fabric.Text(maxscore + " Marks", { fontSize: 12, fill: 'black', top: 49, left: 190, });
         fabricGroup = new fabric.Group([qTittle, qCount, maxscore]);
         return fabricGroup;
-    },
-    addQS: function (event, dataObj) {
+      },
+      addQS: function (event, dataObj) {
         var instance = this;
         var questions = [];
         if(_.isArray(dataObj.data.data)) {
-            dataObj.data.data.forEach(function(question) {
-                questions.push(question);
-            });
+          dataObj.data.data.forEach(function(question) {
+            questions.push(question);
+          });
         }
         var qdata = {};
         qdata.config = {__cdata: JSON.stringify(dataObj.data.config)};
         qdata.data = questions;
 
         if(!ecEditor._.isUndefined(dataObj.callback)){
-            ecEditor.dispatchEvent('delete:invoke');
+          ecEditor.dispatchEvent('delete:invoke');
         }
         ecEditor.dispatchEvent(this.manifest.id + ':create', qdata);
-    },
-    toECML: function () {
+      },
+      toECML: function () {
         var instance = this;
 
         // Generate the questionSet ECML by using the basePlugin `toECML` function.
         var questionSetECML = this._super();
 
         if (_.isArray(instance.data)) {
-            instance.data.forEach(function (question) {
-                if (_.isUndefined(questionSetECML[instance._questionPlugin])) questionSetECML[instance._questionPlugin] = [];
+          instance.data.forEach(function (question) {
+            if (_.isUndefined(questionSetECML[instance._questionPlugin])) questionSetECML[instance._questionPlugin] = [];
 
                 // Build Question ECML for each question that is added.
                 var questionECML = {
-                    id: UUID(),
-                    type: question.data.type,
-                    pluginId: question.data.plugin.id,
-                    pluginVer: question.data.plugin.version,
-                    templateId: question.data.plugin.templateId,
-                    data: {__cdata: JSON.stringify(question.data.data)},
-                    config: {__cdata: JSON.stringify(question.config)}
+                  id: UUID(),
+                  type: question.data.type,
+                  pluginId: question.data.plugin.id,
+                  pluginVer: question.data.plugin.version,
+                  templateId: question.data.plugin.templateId,
+                  data: {__cdata: JSON.stringify(question.data.data)},
+                  config: {__cdata: JSON.stringify(question.config)}
                 };
 
                 // Instantiate the question unit plugin to add it to <plugin-manifest>
                 ecEditor.instantiatePlugin(question.data.plugin.id, {});
 
                 questionSetECML[instance._questionPlugin].push(questionECML);
-            });
+              });
         }
         return questionSetECML;
-    },
-    getConfig: function() {
+      },
+      getConfig: function() {
         var instance = this;
         var config = instance._super();
         config.title = instance.config.title;
@@ -133,58 +133,59 @@ org.ekstep.contenteditor.basePlugin.extend({
         config.total_items = instance.config.total_items;
 
         return config;
-    },
-    onConfigChange: function(key, value) {
+      },
+      onConfigChange: function(key, value) {
         if (!_.isUndefined(value)) {
-            var itemLength = this.data.length;
-            switch (key) {
-                case 'title':
-                    this.config.title = value;
-                    this.editorObj._objects[1]._objects[0].setText(value.toUpperCase());
-                    break;
-                case 'total_items':
-                    this.config.total_items = value;
-                    this.editorObj._objects[1]._objects[1].setText(value + "/" + itemLength + "Questions,");
-                    break;
-                case 'max_score':
-                    this.config.max_score = value;
-                    this.editorObj._objects[1]._objects[2].setText(value + "Marks");
-                    break;
-                case 'shuffle_questions':
-                    this.config.shuffle_questions = value;
-                    break;
-                case 'show_feedback':
-                    this.config.show_feedback = value;
-                    break;
-                case 'optionShuffle':
-                    this.config.optionShuffle = value;
-                    break;
-                case 'questionbankbrowser':
-                    ecEditor.dispatchEvent('delete:invoke');
-                    break;
-                }
+          var itemLength = this.data.length;
+          switch (key) {
+            case 'title':
+            this.config.title = value;
+            this.editorObj._objects[1]._objects[0].setText(value.toUpperCase());
+            break;
+            case 'total_items':
+            this.config.total_items = value;
+            this.editorObj._objects[1]._objects[1].setText(value + "/" + itemLength + "Questions,");
+            break;
+            case 'max_score':
+            this.config.max_score = value;
+            this.editorObj._objects[1]._objects[2].setText(value + "Marks");
+            break;
+            case 'shuffle_questions':
+            this.config.shuffle_questions = value;
+            break;
+            case 'show_feedback':
+            this.config.show_feedback = value;
+            break;
+            case 'optionShuffle':
+            this.config.optionShuffle = value;
+            break;
+            case 'questionbankbrowser':
+            ecEditor.dispatchEvent('delete:invoke');
+            break;
+          }
         }
         ecEditor.render();
         ecEditor.dispatchEvent('object:modified', {
-            target: ecEditor.getEditorObject()
+          target: ecEditor.getEditorObject()
         });
-    },
+      },
     /**
      *
      * open question bank.
      * @memberof questionset
      *
      */
-    openQuestionBank: function(event, callback) {
-        var data;
-        if(ecEditor._.isUndefined(callback)){
-            data = undefined;
-        }else{
-            callback = callback.callback;
-            data = {data : ecEditor.getCurrentObject().data, config : ecEditor.getCurrentObject().config};
-        }
+     openQuestionBank: function(event, callback) {
+      var data;
+      if(ecEditor._.isUndefined(callback)){
+        data = undefined;
+      }else{
+        callback = callback.callback;
+        data = {data : ecEditor.getCurrentObject().data, config : ecEditor.getCurrentObject().config};
+      }
 
-        ecEditor.dispatchEvent('org.ekstep.questionbank:showpopup', {callback: callback, data: data});
+      ecEditor.dispatchEvent('org.ekstep.questionbank:showpopup', {callback: callback, data: data});
     }
-});
+  });
 //# sourceURL=questionsetPlugin.js
+
