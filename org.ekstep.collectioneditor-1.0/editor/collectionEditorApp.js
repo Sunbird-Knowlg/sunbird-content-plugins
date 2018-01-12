2d4aa8fcfb0e924fb6985982a4ac6949c634557c
akashgupta9990 <akash.gupta@tarento.com>
2018-01-12 17:12:26 +0530

    Issue #841 feat: Added Collection context UI & backend integration

--- org.ekstep.collectioneditor-1.0/editor/collectionEditorApp.js
+++ org.ekstep.collectioneditor-1.0/editor/collectionEditorApp.js
@@ -8,6 +8,7 @@ angular.module('org.ekstep.collectioneditor', ["Scope.safeApply"]).controller('m
     $scope.selectedObjectType = undefined;
     $scope.nodeFilter = "";
     $scope.expandNodeFlag = true;
+    $scope.defaultImage = ecEditor.resolvePluginResource("org.ekstep.collectioneditor", "1.0", "assets/default.png");
     $scope.getObjectType = function(objectType) {
         return _.find(objectType, function(type) {
             return type == $scope.selectedObjectType
@@ -20,11 +21,31 @@ angular.module('org.ekstep.collectioneditor', ["Scope.safeApply"]).controller('m
     };
 
     $scope.setSelectedNode = function(event, data) {
+        $scope.collectionData = data;
         if (data.data.objectType) {
             $scope.selectedObjectType = data.data.objectType
             $scope.$safeApply();
         }
     };
+
+    $scope.deleteNode = function(node, event) {
+        if (!node.data.root) {
+            ecEditor.getService('popup').open({
+                template: '<div class="ui mini modal active" style="top:' + event.clientY + 'px;right:' + event.clientX + 'px;"><div class="content"><span class="custom-modal-heading">Are you sure you want to delete this content?</span> <i class="close large icon floatContentRight" ng-click="closeThisDialog()"></i><p class="custom-modal-content">All content within this folder will also be deleted from this textbook.</p><button class="ui red button" ng-click="confirm()">YES,  DELETE</button></div></div>',
+                // '<div class="ui icon negative message remove-unit-popup"><i class="close icon" ></i><div class="content"><div class="header"><i class="fa fa-exclamation-triangle"></i> Do you want to remove this?</div><div class="remove-unit-buttons" style="padding-right:0; text-align:right;"><div class="ui red button button-overrides" id="remove-yes-button" >Yes</div><div class="ui basic primary button button-overrides" id="remove-no-button" >No</div></div></div></div>',
+                controller: ["$scope", function($scope) {
+                    $scope.confirm = function() {
+                        node.remove();
+                        $scope.closeThisDialog();
+                        delete org.ekstep.collectioneditor.cache.nodesModified[node.data.id];
+                        ecEditor.dispatchEvent("org.ekstep.collectioneditor:node:removed", node.data.id);
+                    };
+                }],
+                plain: true,
+                showClose: false
+            });
+        }
+    }
     //Header scope starts
     $scope.headers = [];
 
