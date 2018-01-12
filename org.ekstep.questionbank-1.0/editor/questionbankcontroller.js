c6e5d2f54f8a343d0b77c4c8c3faffa81f9d45fa
“Gourav <gourav_m@tekditechnologies.com>
2018-01-12 20:58:14 +0530

    Issue #840 feat: API Integration for Question bank plugin

--- org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
+++ org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
@@ -311,29 +311,29 @@ angular.module('createquestionapp', [])
     }
 
     $scope.previewItem = function(question, bool) {
-      // var prevData={};
-      // prevData.config = question.config;
-      // prevData.data=question.data.data;
-      // prevData.pluginId=question.data.plugin.id;
-      // prevData.pluginVer=question.data.plugin.version;
-      // prevData.templateId=question.data.plugin.templateId;
-      // prevData.id = question.questionId;
-      // prevData.type="unit";
-      // ecEditor.instantiatePlugin(prevData.id);
-      // var data = {
-      //   "org.ekstep.questionset": {
-      //     "config": {
-      //       'questionCount': 1,
-      //       'isShuffle': true
-      //     },
-      //     "org.ekstep.question": []
-      //   }
-      // }
-      // data["org.ekstep.questionset"]['org.ekstep.question'].push(prevData)
-      // var confData={"contentBody":{},"parentElement":true,"element":"#itemIframe"};
-      // var questionSetInstance = ecEditor.instantiatePlugin('org.ekstep.questionset.preview');
-      // confData.contentBody = questionSetInstance.getQuestionPreviwContent(data['org.ekstep.questionset']);
-      // ecEditor.dispatchEvent("atpreview:show", confData);
+      var prevData={};
+      prevData.config = question.config;
+      prevData.data=question.data.data;
+      prevData.pluginId=question.data.plugin.id;
+      prevData.pluginVer=question.data.plugin.version;
+      prevData.templateId=question.data.plugin.templateId;
+      prevData.id = question.questionId;
+      prevData.type="unit";
+      ecEditor.instantiatePlugin(prevData.id);
+      var data = {
+        "org.ekstep.questionset": {
+          "config": {
+            'questionCount': 1,
+            'isShuffle': true
+          },
+          "org.ekstep.question": []
+        }
+      }
+      data["org.ekstep.questionset"]['org.ekstep.question'].push(prevData)
+      var confData={"contentBody":{},"parentElement":true,"element":"#itemIframe"};
+      var questionSetInstance = ecEditor.instantiatePlugin('org.ekstep.questionset.preview');
+      confData.contentBody = questionSetInstance.getQuestionPreviwContent(data['org.ekstep.questionset']);
+      ecEditor.dispatchEvent("atpreview:show", confData);
     }
 
     $scope.cancel = function() {
