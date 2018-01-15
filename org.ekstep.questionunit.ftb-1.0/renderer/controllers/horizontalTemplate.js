7ca0cbee8539d730226d950536d9c73cdc64eae0
Sachin <sachin.kumar@goodworklabs.com>
2018-01-15 16:27:01 +0530

    Issue #845 feat: ftb plugin in render

--- org.ekstep.questionunit.ftb-1.0/renderer/controllers/horizontalTemplate.js
+++ org.ekstep.questionunit.ftb-1.0/renderer/controllers/horizontalTemplate.js
@@ -1,66 +1,78 @@
-// TODO: Controller for horizontalTemplate.html
 'use strict';
 angular.module('FTBRendererApp', []).controller("FTBRendererController", function($scope) {
-  var ctrl = this;
-  $scope.questionObj = {
-    "question": "Choose the best",
-    "options": [{
-      "value": {
-        "id": "a",
-        "type": "mixed",
-        "text": "This is an Apple This is an Apple This is an Apple This is an Apple",
-        "audio": "leg_aud",
-        "image": "home_img"
+  $scope.showTemplate = true;
+  $scope.question;
+  $scope.ftbAnswer;
+  $scope.qcquestion = true;
+  $scope.qcblank = true;
+  $scope.cssPath = org.ekstep.pluginframework.pluginManager.resolvePluginResource("org.ekstep.questionunit.ftb", "1.0", "renderer/styles/horizontalTemplate.css");
+  $scope.init = function() {
+    $scope.pluginInstance = EkstepRendererAPI.getPluginObjs("org.ekstep.questionunit.ftb");
+    // To show template/plugin  
+        /**
+     * renderer:questionunit.ftb:dispatch an event in question set with question data.
+     * @event renderer:questionunit.ftb:dispatch
+     * @memberof org.ekstep.questionunit.ftb
+     */
+    EkstepRendererAPI.addEventListener($scope.pluginInstance._manifest.id + ":show", function(event, question) {
+      $scope.question = event.target;
+      var questionData = JSON.parse($scope.question._currentQuestion.data);
+      $scope.questionObj = questionData;
+      $scope.showTemplate = true;
+      $scope.safeApply();
+    });
+    /**
+     * renderer:questionunit.ftb:hide template on question set navigation.
+     * @event renderer:questionunit.ftb:dispatch
+     * @memberof org.ekstep.questionunit.ftb
+     */
+    EkstepRendererAPI.addEventListener($scope.pluginInstance._manifest.id + ":hide", function(event) {
+      $scope.showTemplate = false;
+      $scope.safeApply();
+    });
+    /**
+     * renderer:questionunit.ftb:question set call evalution using plugin instance.
+     * @event renderer:questionunit.ftb:click
+     * @memberof org.ekstep.questionunit.ftb
+     */
+    EkstepRendererAPI.addEventListener($scope.pluginInstance._manifest.id + ":evaluate", function(event) {
+      $scope.evaluate();
+      $scope.safeApply();
+    });
 
-      }
-    }, {
-      "value": {
-        "id": "b",
-        "type": "mixed",
-        "text": "Mango",
-        "audio": "earspell_aud",
-        "image": ""
-      }
-    }, {
-      "value": {
-        "id": "c",
-        "type": "mixed",
-        "text": "Orange",
-        "audio": "earspell_aud",
-        "image": ""
-      }
-    }, {
-      "value": {
-        "id": "d",
-        "type": "mixed",
-        "text": "Banana",
-        "audio": "earspell_aud",
-        "image": ""
-
-      },
-
-      "answer": true
-    }, {
-      "value": {
-        "id": "e",
-        "type": "mixed",
-        "text": "Mango1",
-        "audio": "earspell_aud",
-        "image": ""
-      }
-    }, {
-      "value": {
-        "id": "f",
-        "type": "mixed",
-        "text": "Orange1",
-        "audio": "earspell_aud",
-        "image": ""
-      }
-    }, ]
   }
-  ctrl.selectedValue = function(value, event) {
-    console.log(value, event.target.id);
-    $("#" + event.target.id).parent().css("border", "3px solid #166ca6").css("z-index", "4").css("box-shadow", "2px 2px 2px 2px #93bdda");
-    $("#" + event.target.id).parent().siblings().css("border", "2px solid #166ca6").css("z-index", "1").css("box-shadow", "none");
+  $scope.init();
+  /**
+     * renderer:questionunit.ftb:show keyboard in device.
+     * @event renderer:questionunit.ftb:click
+     * @memberof org.ekstep.questionunit.ftb
+     */
+  window.addEventListener('native.keyboardshow', function(e) {
+    $scope.qcquestion = false;
+    $scope.safeApply();
+  });
+   /**
+     * renderer:questionunit.ftb:hide keyboard in device.
+     * @event renderer:questionunit.ftb:click
+     * @memberof org.ekstep.questionunit.ftb
+     */
+  window.addEventListener('native.keyboardhide', function() {
+    $scope.qcquestion = true;
+    $scope.qcblank = true;
+    $scope.safeApply();
+  });
+
+  $scope.evaluate = function() {
+    var correctAnswer = false;
+    if ($scope.questionObj.answers[0].text.toLowerCase().replace(/ /g, '') === $scope.ftbAnswer.toLowerCase().replace(/ /g, '')) {
+      correctAnswer = true;
+    }
+    if (correctAnswer) {
+      alert("right ftbAnswer");
+
+    } else {
+      alert("wrong ftbAnswer");
+    }
   }
 });
+//# sourceURL=questionunitFtbRenderereTmpPlugin.js
