8ad9d0dfaa07bbd783ed567a0a821ebe8f25eb41
JagadishPujari <jagadish.pujari@tarento.com>
2018-01-31 00:35:27 +0530

    Issue #835 Fix: Show Good and try again pop up rendere side

--- org.ekstep.questionset-1.0/renderer/controller/questionset_ctrl.js
+++ org.ekstep.questionset-1.0/renderer/controller/questionset_ctrl.js
@@ -21,25 +21,53 @@ angular.module('genie-canvas')
 	})
 .directive('qsGoodJob', function($rootScope) {
 	return {
-		restrict: 'E',
+		restrict: 'AE',
 		template: '<div class="popup"> <div class="popup-overlay" ng-click="hidePopup()"></div> <div class="popup-full-body"> <div class="font-lato assess-popup assess-goodjob-popup"> <div class="correct-answer" style=" text-align: center;"> <div class="banner"> <img ng-src="assets/icons/banner3.png" height="100%" width="100%" src="assets/icons/banner3.png"> </div> <div class="sign-board"> <img ng-src="assets/icons/check.png" id="correctButton" width="40%" src="assets/icons/check.png"> </div> </div> <div id="popup-buttons-container"> <div ng-click="hidePopup();moveNextStage();" class="primary center button ng-binding">Next</div> </div> </div> </div> </div>',
-		controller: function($scope, $rootScope, $timeout) { }
+		link: function(scope, element, attrs) {
+			EkstepRendererAPI.addEventListener('renderer:load:popup:goodJob', function (event) {
+				element.show();
+			});
+			scope.hidePopup = function(){
+				element.hide();
+			}
+			scope.moveNextStage = function(){
+				EkstepRendererAPI.dispatchEvent('renderer:nextStage');
+				scope.hidePopup();
+				element.hide();
+			}
+		}
 	}
 })
 .directive('qsTryAgain', function($rootScope) {
 	return {
-		restrict: 'E',
-		template: '<div class="popup"> <div class="popup-overlay" ng-click="hidePopup()"></div> <div class="popup-full-body"> <div class="font-lato assess-popup assess-tryagain-popup"> <div class="wrong-answer" style=" text-align: center;"> <div class="banner"> <img ng-src="assets/icons/banner2.png" height="100%" width="100%" src="assets/icons/banner2.png"> </div> <div class="sign-board"><img ng-src="assets/icons/incorrect.png" width="40%" id="incorrectButton" src="assets/icons/incorrect.png"> </div> </div> <div id="popup-buttons-container"> <div ng-click="hidePopup();moveNextStage();" class="left button ng-binding">Next</div> <div ng-click="tryAgainSameQ();" class="right primary button ng-binding">Try Again</div> </div> </div> </div> </div>'
+		restrict: 'AE',
+		template: '<div class="popup"> <div class="popup-overlay" ng-click="tryAgainHidePopup()"></div> <div class="popup-full-body"> <div class="font-lato assess-popup assess-tryagain-popup"> <div class="wrong-answer" style=" text-align: center;"> <div class="banner"> <img ng-src="assets/icons/banner2.png" height="100%" width="100%" src="assets/icons/banner2.png"> </div> <div class="sign-board"><img ng-src="assets/icons/incorrect.png" width="40%" id="incorrectButton" src="assets/icons/incorrect.png"> </div> </div> <div id="popup-buttons-container"> <div ng-click="tryAgainHidePopup();moveNextStage();" class="left button ng-binding">Next</div> <div ng-click="tryAgainSameQ();" class="right primary button ng-binding">Try Again</div> </div> </div> </div> </div>',
+		link: function(scope, element, attrs) {
+			EkstepRendererAPI.addEventListener('renderer:load:popup:tryAgain', function (event) {
+				element.show();
+			});
+			scope.tryAgainHidePopup = function(){
+				element.hide();
+			}
+			scope.moveNextStage = function(){
+				EkstepRendererAPI.dispatchEvent('renderer:nextStage');
+				scope.tryAgainHidePopup();
+				element.hide();
+			}
+			scope.tryAgainSameQ = function(){
+				scope.tryAgainHidePopup();
+				element.hide();
+			}
+		}
 	}
 })
 .controllerProvider.register("questionsetctrl", function($scope, $ocLazyLoad,$compile) {
-	$scope.showOverlayGoodJob = false;
-	$scope.showOverlayTryAgain = false;
+
 /**
  * Event handler to dynamically load HTML file and add it to DOM.
  * @event "renderer:load:html"
  */
- 	EkstepRendererAPI.addEventListener('renderer:load:html', function (event) {
+ EkstepRendererAPI.addEventListener('renderer:load:html', function (event) {
  	var data = event.target;
  	if (data.path) {
  		$ocLazyLoad.load([{type: 'html', path: data.path}]).then(function () {
@@ -53,43 +81,22 @@ angular.module('genie-canvas')
  			}
  		});
  	}
-	});
+ });
 
 /**
  * Event handler to dynamically load a JS file.
  * @event "renderer:load:js"
  */
- 	EkstepRendererAPI.addEventListener('renderer:load:js', function (event) {
-	 	var data = event.target;
-	 	if (data.path) {
-	 		$ocLazyLoad.load({type: 'js', path: data.path}).then(function () {
-	 			if (_.isFunction(data.callback)) data.callback(data);
-	 		});
-	 	}
-	 	$scope.safeApply();
-	});
+ EkstepRendererAPI.addEventListener('renderer:load:js', function (event) {
+ 	var data = event.target;
+ 	if (data.path) {
+ 		$ocLazyLoad.load({type: 'js', path: data.path}).then(function () {
+ 			if (_.isFunction(data.callback)) data.callback(data);
+ 		});
+ 	}
+ 	$scope.safeApply();
+ });
 
-	EkstepRendererAPI.addEventListener('renderer:load:popup:goodJob', function (event) {
-		$scope.showOverlayGoodJob = true;
-		$scope.safeApply();
-	});
-   
-	EkstepRendererAPI.addEventListener('renderer:load:popup:tryAgain', function (event) {
-		$scope.showOverlayTryAgain = true;  
-		$scope.safeApply(); 
-	});
-   
-	$scope.moveNextStage = function(){
-		EkstepRendererAPI.dispatchEvent('renderer:nextStage');
-		$scope.showOverlayGoodJob = false;
-		$scope.showOverlayTryAgain = false;	
-		$scope.safeApply();	
-	}
-	$scope.tryAgainSameQ = function(){
-		$scope.showOverlayGoodJob = false;
-		$scope.showOverlayTryAgain = false;	
-		$scope.safeApply();
-	}
 });
 
 //# sourceURL=questionSetCtrl.js
