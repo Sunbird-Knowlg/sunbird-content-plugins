a9fabba61173b245183180fb09ef0177f4f615c1
akshaya <akshaya.rathinavel@tarento.com>
2018-04-06 14:35:48 +0530

    Issue #805 feat: Question text styling for question unit plugins

--- org.ekstep.questionunit-1.0/editor/plugin.js
+++ org.ekstep.questionunit-1.0/editor/plugin.js
@@ -6,8 +6,10 @@
  * @author Manoj Chandrashekar <manoj.chandrashekar@tarento.com>
  */
 org.ekstep.contenteditor.questionUnitPlugin = org.ekstep.contenteditor.basePlugin.extend({
-    type: "org.ekstep.contenteditor.questionUnitPlugin",
-
-    // TODO: Interfaces
+	type: "org.ekstep.contenteditor.questionUnitPlugin",
+	initialize: function() {
+		CKEDITOR.basePath = ecEditor.resolvePluginResource(this.manifest.id, this.manifest.ver, "editor/libs/");
+	}
+	// TODO: Interfaces
 });
 //# sourceURL=questionsetPlugin.js
--- org.ekstep.questionunit.ftb-1.0/editor/controllers/horizontal_controller.js
+++ org.ekstep.questionunit.ftb-1.0/editor/controllers/horizontal_controller.js
@@ -5,61 +5,72 @@
  */
 
 angular.module('ftbApp', [])
- .controller('ftbQuestionFormController', ['$scope', '$rootScope', function($scope, $rootScope) {
+  .controller('ftbQuestionFormController', ['$scope', '$rootScope', function($scope, $rootScope) {
 
-  $scope.formVaild = false;
-  $scope.ftbFormData = {
-    'question': { 'text': '', 'image': '', 'audio': '' },
-    'answer': [],
-    'parsedQuestion': { 'text': '', 'image': '', 'audio': '' }
-  };
+    $scope.formVaild = false;
+    $scope.ftbFormData = {
+      'question': { 'text': '', 'image': '', 'audio': '' },
+      'answer': [],
+      'parsedQuestion': { 'text': '', 'image': '', 'audio': '' }
+    };
+    
+    var questionInput = CKEDITOR.replace('ftbQuestion', {
+      customConfig: CKEDITOR.basePath + "config.js",
+      skin: 'moono-lisa,' + CKEDITOR.basePath + "skins/moono-lisa/",
+      contentsCss: CKEDITOR.basePath + "contents.css"
+    });
+    questionInput.on('change', function() {
+      $scope.ftbFormData.question.text = this.getData();
+    });
+    $scope.init = function() {
+      $('.menu .item').tab();
+      $('.ui.dropdown').dropdown({ useLabels: false });
 
-  $scope.init = function() {
-    $('.menu .item').tab();
-    $('.ui.dropdown').dropdown({ useLabels: false });
-
-    if (!ecEditor._.isUndefined($scope.questionEditData)) {
-      var data = $scope.questionEditData.data;
-      $scope.ftbFormData.question = data.question;
-    }
-
-    $scope.$parent.$on('question:form:val', function(event) {
-      var regexForAns = /(?:^|)\[\[(.*?)(?:\]\]|$)/g;
-      var index = 0;
-      $scope.ftbFormData.answer = $scope.getMatches($scope.ftbFormData.question.text, regexForAns, 1).map(function (a) {
-        return a.toLowerCase().trim();
-      });
-      if ($scope.formValidation()) {
-        $scope.ftbFormData.parsedQuestion.text = $scope.ftbFormData.question.text.replace(/\[\[.*?\]\]/g,function(a, b){
-          index = index +1;
-          return '<input type="text" class="ans-field" id=ans-field' + index + '>';
-        })
-        $scope.$emit('question:form:valid', $scope.ftbFormData);
-      } else {
-        $scope.$emit('question:form:inValid', $scope.ftbFormData);
+      if (!ecEditor._.isUndefined($scope.questionEditData)) {
+        var data = $scope.questionEditData.data;
+        $scope.ftbFormData.question = data.question;
       }
-    })
-  }
 
-  
-
-  $scope.getMatches = function(string, regex, index) {
-    index || (index = 1); // default to the first capturing group
-    var matches = [];
-    var match;
-    while (match = regex.exec(string)) {
-      matches.push(match[index]);
+      $scope.$parent.$on('question:form:val', function(event) {
+        var regexForAns = /(?:^|)\[\[(.*?)(?:\]\]|$)/g;
+        var index = 0;
+        $scope.ftbFormData.answer = $scope.getMatches($scope.ftbFormData.question.text, regexForAns, 1).map(function(a) {
+          return a.toLowerCase().trim();
+        });
+        if ($scope.formValidation()) {
+          $scope.ftbFormData.parsedQuestion.text = $scope.ftbFormData.question.text.replace(/\[\[.*?\]\]/g, function(a, b) {
+            index = index + 1;
+            return '<input type="text" class="ans-field" id=ans-field' + index + '>';
+          })
+          $scope.$emit('question:form:valid', $scope.ftbFormData);
+        } else {
+          $scope.$emit('question:form:inValid', $scope.ftbFormData);
+        }
+      })
+      ckeditor.editor.on('change', function() {
+        ngModel.$setViewValue(this.getData());
+      });
     }
-    return matches;
-  }
 
-  $scope.formValidation = function() {
-    $scope.submitted=true;
-    var formValid = $scope.ftbForm.$valid && /\[\[.*?\]\]/g.test($scope.ftbFormData.question.text);
-    return (formValid) ? true : false;
-  }
 
-  $scope.generateTelemetry = function(data, event) {
+
+    $scope.getMatches = function(string, regex, index) {
+      index || (index = 1); // default to the first capturing group
+      var matches = [];
+      var match;
+      while (match = regex.exec(string)) {
+        matches.push(match[index]);
+      }
+      return matches;
+    }
+
+    $scope.formValidation = function() {
+      $scope.submitted = true;
+      var formValid = $scope.ftbForm.$valid && /\[\[.*?\]\]/g.test($scope.ftbFormData.question.text);
+      return (formValid) ? true : false;
+    }
+
+    $scope.generateTelemetry = function(data, event) {
       if (data) ecEditor.getService('telemetry').interact({
         "type": data.type,
         "subtype": data.subtype,
@@ -77,5 +88,5 @@ angular.module('ftbApp', [])
       })
     }
 
-}]);
+  }]);
 //# sourceURL=horizontalFTB.js
