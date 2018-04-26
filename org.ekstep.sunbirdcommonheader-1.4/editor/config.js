76f28e25fbea388fd2b643c8fda3586aa9416f1f
Manjunath Davanam <manjunathd@ilimi.in>
2018-04-26 12:34:38 +0530

    Issue ekstep/Collection-Editor#119 fix: sunbird header plugin bump up to 1.4 ver and content-editor functions plugin bump up to 1.3 ver

---
+++ org.ekstep.sunbirdcommonheader-1.4/editor/config.js
@@ -0,0 +1,112 @@
+var checkListConfigurations = {
+    "reject": {
+        "title": "Request Changes Checklist",
+        "subtitle": "Please select the items that need changes:",
+        "contents": [{
+                "name": "Appropriateness Check",
+                "index": 3,
+                "checkList": [
+                    "Has Hate speech, Abuse, Violence",
+                    "Has profanity or abusive language",
+                    "Has Sexually explicit content, Nudity or Vulgarity",
+                    "Has Discriminatory or Defamatory content",
+                    "Is not suitable for children"
+                ]
+            },
+            {
+                "name": "Content details Check",
+                "index": 2,
+                "checkList": [
+                    " Inappropriate Title",
+                    "Inappropriate Description",
+                    "Correct Grade, Subject, Medium",
+                    "Incorrect Board",
+                    "Incorrect Grade, Subject, Medium",
+                    "Incorrect  Resource Type",
+                    "Incorrect Concepts",
+                    "Irrelevant Keywords",
+                    "Objectives unclear",
+                    "Insufficient content for objectives",
+                    " Duration of content is too long for the objectives"
+                ]
+            },
+            {
+                "name": "Usability Check",
+                "index": 1,
+                "checkList": [
+                    "Content is NOT rendering well on desktop and App",
+                    "Content DOES NOT have visual clarity",
+                    "Content  DOES NOT play correctly",
+                    "Language is NOT simple to understand",
+                    "Spelling mistakes found in text used",
+                    " Audio (if any) is inaudible and not easy to understand"
+                ]
+            }
+        ],
+        "comments": {
+            "required": true,
+            "label": "Comments"
+        },
+        "buttons": {
+            "cancel": "Cancel",
+            "proceed": "Request changes"
+        }
+    },
+    "publish": {
+        "title": "Publish Checklist",
+        "subtitle": "Please review and accept that following before you publish:",
+        "contents": [{
+                "name": "Appropriateness Check",
+                "index": 1,
+                "checkList": [
+                    " Has Hate speech, Abuse, Violence",
+                    " Has profanity or abusive language",
+                    "Has Sexually explicit content, Nudity or Vulgarity",
+                    "Has Discriminatory or Defamatory content",
+                    "Is not suitable for children"
+                ]
+            },
+            {
+                "name": "Content details Check",
+                "index": 3,
+                "checkList": [
+                    " Inappropriate Title",
+                    "Inappropriate Description",
+                    "Correct Grade, Subject, Medium",
+                    "Incorrect Board",
+                    "Incorrect Grade, Subject, Medium",
+                    "Incorrect  Resource Type",
+                    "Incorrect Concepts",
+                    "Irrelevant Keywords",
+                    "Objectives unclear",
+                    "Insufficient content for objectives",
+                    " Duration of content is too long for the objectives"
+                ]
+            },
+            {
+                "name": "Usability Check",
+                "index": 2,
+                "checkList": [
+                    "Content is NOT rendering well on desktop and App",
+                    "Content DOES NOT have visual clarity",
+                    "Content  DOES NOT play correctly",
+                    "Language is NOT simple to understand",
+                    "Spelling mistakes found in text used",
+                    " Audio (if any) is inaudible and not easy to understand"
+                ]
+            }
+        ],
+        "comments": {
+            "required": false,
+            "label": "Comments"
+        },
+        "buttons": {
+            "cancel": "Cancel",
+            "proceed": "Publish"
+        }
+    },
+    "read": {
+        "title": "Review Comments",
+        "subtitle": ""
+    }
+}
