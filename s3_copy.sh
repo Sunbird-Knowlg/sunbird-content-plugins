5b541aa7d78727dafdb1d13710379e34d73db362
Manjunath Davanam <manjunathd@ilimi.in>
2018-05-29 11:37:12 +0530

    Issue ekstep/Content-Editor#1148 fix: Plugin version bump up

--- s3_copy.sh
+++ s3_copy.sh
@@ -59,7 +59,7 @@ declare -a coreplugins=("org.ekstep.activitybrowser-1.1"
                 "org.ekstep.iterator-1.0"
                 "org.ekstep.navigation-1.0"
                 "org.ekstep.breadcrumb-1.0"
-                "org.ekstep.sunbirdmetadata-1.0"
+                "org.ekstep.sunbirdmetadata-1.1"
                 "org.ekstep.editcontentmeta-1.1"
                 "org.ekstep.metadata-1.1"
                 "org.ekstep.collectionkeyboardshortcuts-1.0");
