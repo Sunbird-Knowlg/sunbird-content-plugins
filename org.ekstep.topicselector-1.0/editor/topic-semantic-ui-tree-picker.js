9c6b195a8d2a73f825f530f7cd6bc4119e2cfe5a
Gourav More <gourav_m@tekditechnologies.com>
2018-06-08 19:20:01 +0530

    Issue #1086 fix: Resources : 'Remove All' is not working in 'Topic Tree' pop up

--- org.ekstep.topicselector-1.0/editor/topic-semantic-ui-tree-picker.js
+++ org.ekstep.topicselector-1.0/editor/topic-semantic-ui-tree-picker.js
@@ -8,16 +8,16 @@ var topicModal;
         nodes = [];
         tabs = {};
         $("#" + options.nodeName).length == 0 ? '' : $("#" + options.nodeName).remove();
-        modal = $("<div id="+ options.nodeName +" class=\"ui tree-picker small modal\">\n  <div class=\"header\">\n    " + options.name + "\n\n    <div class=\"ui menu\">\n      <a class=\"active tree item\">\n        <i class=\"list icon\"></i> Topics\n      </a>\n      <a class=\"picked item\">\n        <i class=\"checkmark icon\"></i> Selected Topics <span class=\"count\"></span>\n      </a>\n    </div>\n  </div>\n  <div class=\"ui search form\">\n    <div class=\"field\">\n      <div class=\"ui icon input\">\n        <input type=\"text\" placeholder=\"Search\">\n        <i class=\"search icon\"></i>\n      </div>\n    </div>\n  </div>\n  <div class=\"content\">\n <div class=\"ui warning hidden message\"><i class=\"close icon\"></i><div class=\"header\">No topic/subtopic is available with provided Board, Grade, and Subject combinations.</div>Please verify the provided values for Board, Grade, and Subject</div><div class=\"ui active inverted dimmer\"><div class=\"ui text loader\">Loading data</div></div>\n    <div class=\"tree-tab\">\n      <div style=\"height: 400px\"></div>\n    </div>\n\n    <div class=\"search-tab\">\n    </div>\n\n    <div class=\"picked-tab\">\n    </div>\n  </div>\n  <div class=\"actions\">\n    <a class=\"pick-search\"><i class=\"checkmark icon\"></i> Choose All</a>\n    <a class=\"unpick-search\"><i class=\"remove icon\"></i> Remove All</a>\n    <a class=\"unpick-picked\"><i class=\"remove icon\"></i> Remove All</a>\n    \n    <a class=\"ui button close\">Cancel</a>\n<a class=\"ui blue button accept\">Done</a>\n  </div>\n</div>").modal({
+        modal = $("<div id="+ options.nodeName +" class=\"ui tree-picker small modal\">\n  <div class=\"header\">\n    " + options.name + "\n\n    <div class=\"ui menu\">\n      <a class=\"active tree item\">\n        <i class=\"list icon\"></i> Topics\n      </a>\n      <a class=\"picked item\">\n        <i class=\"checkmark icon\"></i> Selected Topics <span class=\"count\"></span>\n      </a>\n    </div>\n  </div>\n  <div class=\"ui search form\">\n    <div class=\"field\">\n      <div class=\"ui icon input\">\n        <input type=\"text\" placeholder=\"Search\">\n        <i class=\"search icon\"></i>\n      </div>\n    </div>\n  </div>\n  <div class=\"content\">\n <div class=\"ui warning hidden message\"><i class=\"close icon\"></i><div class=\"header\">No topic/subtopic is available with provided Board, Grade, and Subject combinations.</div>Please verify the provided values for Board, Grade, and Subject</div><div class=\"ui active inverted dimmer\"><div class=\"ui text loader\">Loading data</div></div>\n    <div class=\"topic-tree-tab\">\n      <div style=\"height: 400px\"></div>\n    </div>\n\n    <div class=\"topic-search-tab\">\n    </div>\n\n    <div class=\"topic-picked-tab\">\n    </div>\n  </div>\n  <div class=\"actions\">\n    <a class=\"pick-search\"><i class=\"checkmark icon\"></i> Choose All</a>\n    <a class=\"unpick-search\"><i class=\"remove icon\"></i> Remove All</a>\n    <a class=\"unpick-picked\"><i class=\"remove icon\"></i> Remove All</a>\n    \n    <a class=\"ui button close\">Cancel</a>\n<a class=\"ui blue button accept\">Done</a>\n  </div>\n</div>").modal({
             duration: 200,
             allowMultiple: true
         });
         topicModal = modal;
         count = $('.count', modal);
         tabs = {
-            tree: $('.tree-tab', modal),
-            search: $('.search-tab', modal),
-            picked: $('.picked-tab', modal)
+            tree: $('.topic-tree-tab', modal),
+            search: $('.topic-search-tab', modal),
+            picked: $('.topic-picked-tab', modal)
         };
         actionButtons = {
             pickSearch: $('.actions .pick-search', modal),
@@ -64,6 +64,7 @@ var topicModal;
             widget.on('click', function(e) {
                 console.log('click event received', nodes);
                 modal.modal('show');
+                showTree();
                 if (!nodes.length) {
                     if (config.url) {
                         return loadNodes(config.url, {}, function(nodes) {
@@ -98,13 +99,13 @@ var topicModal;
                 }
             });
             actionButtons.pickSearch.on('click', function(e) {
-                return $('.search-tab .node:not(.picked) .name').trigger('click');
+                return $('.topic-search-tab .node:not(.picked) .name', modal).trigger('click');
             });
             actionButtons.unpickSearch.on('click', function(e) {
-                return $('.search-tab .node.picked .name').trigger('click');
+                return $('.topic-search-tab .node.picked .name', modal).trigger('click');
             });
             actionButtons.unpickPicked.on('click', function(e) {
-                return $('.picked-tab .node.picked .name').trigger('click');
+                return $('.topic-picked-tab .node.picked .name', modal).trigger('click');
             });
             $('.menu .tree', modal).on('click', function(e) {
                 return showTree();
@@ -213,7 +214,7 @@ var topicModal;
             tabs.tree.hide();
             tabs.search.hide();
             modal.attr('data-mode', 'picked');
-            return initializeNodeListForSelected(list);
+            return initializeNodeList(list);
         };
         renderTree = function(nodes, css) {
             var i, len, node, nodeElement, tree;
@@ -258,55 +259,24 @@ var topicModal;
         };
         initializeNodeList = function(tree) {
             return $('.node', tree).each(function() {
-                var content, head, node;
-                node = $(this);
-                clickHead = $('>.head.selectable', node);
-                head = $('>.head', node);
-                content = $('>.content', node);
-                $('>.name', clickHead).on('click', function(e) {
-                    return nodeClicked(node);
-                });
-                if (nodeIsPicked(node)) {
-                    node.addClass('picked');
-                }
-                if (!node.hasClass('childless')) {
-					if (!head.hasClass('selectable')){
-						$(head).on('click', function(e) {
-							node.toggleClass('opened');
-							return content.slideToggle();
-						});
-					}else{
-						$('>.icon', head).on('click', function(e) {
-							node.toggleClass('opened');
-							return content.slideToggle();
-						});
-					}
-                }
-                return updatePickedIds();
+            var content, head, node;
+            node = $(this);
+            head = $('>.head', node);
+            content = $('>.content', node);
+            $('>.name', head).on('click', function(e) {
+              return nodeClicked(node);
             });
-        };
-
-        initializeNodeListForSelected = function(tree) {
-            return $('.node', tree).each(function() {
-                var content, head, node;
-                node = $(this);
-                clickHead = $('>.head', node);
-                head = $('>.head', node);
-                content = $('>.content', node);
-                $('>.name', clickHead).on('click', function(e) {
-                    return nodeClicked(node);
-                });
-                if (nodeIsPicked(node)) {
-                    node.addClass('picked');
-                }
-                if (!node.hasClass('childless')) {
-                    $('>.icon', head).on('click', function(e) {
-                        node.toggleClass('opened');
-                        return content.slideToggle();
-                    });
-                }
-                return updatePickedIds();
-            });
+            if (nodeIsPicked(node)) {
+              node.addClass('picked');
+            }
+            if (!node.hasClass('childless')) {
+              $('>.icon', head).on('click', function(e) {
+                node.toggleClass('opened');
+                return content.slideToggle();
+              });
+            }
+            return updatePickedIds();
+          });
         };
 
         nodeClicked = function(node) {
