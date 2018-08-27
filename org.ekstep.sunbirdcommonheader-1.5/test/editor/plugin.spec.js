e6e0cd4ba6cec31c51e9388765ce7df2b2b3f82b
Akash Gupta <akash.gupta@tarento.com>
2018-08-27 20:44:49 +0530

    Issue #SB-6161 fix: Added test cases for dynamic checklist

--- org.ekstep.sunbirdcommonheader-1.5/test/editor/plugin.spec.js
+++ org.ekstep.sunbirdcommonheader-1.5/test/editor/plugin.spec.js
@@ -1,5 +1,5 @@
 describe("Sunbird header plugin:", function() {
-    var manifest, ctrl, $scope, pluginInstance;
+    var manifest, ctrl, $scope, pluginInstance, checklistJSON, checklistEmptyResponse;
     beforeAll(function(done) {
         CollectionEditorTestFramework.init(function() {
             manifest = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.sunbirdcommonheader");
@@ -13,6 +13,8 @@ describe("Sunbird header plugin:", function() {
             ecEditor.getCurrentStage = jasmine.createSpy().and.callFake(function() {
                 return { id: '5437859-543758937' }
             });
+            checklistJSON = JSON.parse('{"type":"content","subType":"resource","action":"requestForChangesChecklist","rootOrgId":"*","data":{"templateName":"defaultTemplate","action":"requestforchangeschecklist","fields":[{"title":"Please tick the reasons for requesting changes and provide detailed comments:","otherReason":"Other Issue(s) (if there are any other issues, tick this and provide details in the comments box)","contents":[{"name":"Appropriateness","checkList":["Has Hate speech, Abuse, Violence, Profanity","Has Sexual content, Nudity or Vulgarity","Has Discriminatory or Defamatory content","Is not suitable for children"]},{"name":"Content details","checkList":["Inappropriate Title or Description","Incorrect Board, Grade, Subject or Medium","Inappropriate tags such as Resource Type or Concepts","Irrelevant Keywords"]},{"name":"Usability","checkList":["Content is NOT playing correctly","CANNOT see the content clearly on Desktop and App","Audio is NOT clear or NOT easy to understand","Spelling mistakes found in text used","Language is NOT simple to understand"]}]}]}}');
+            checklistEmptyResponse = JSON.parse('{"id":"api.form.read","ver":"1.0","ts":"2018-08-27T15:13:33.427Z","params":{"resmsgid":"c363f430-aa0b-11e8-b56f-3df6698f6418","msgid":"c3574a00-aa0b-11e8-a60e-1b24840be16f","status":"successful","err":null,"errmsg":null},"responseCode":"OK","result":{"tenantPreference":[]}}')
             done();
         })
     })
@@ -145,6 +147,70 @@ describe("Sunbird header plugin:", function() {
             spyOn($scope, "publishContent").and.callThrough();
             $scope.publishContent();
             expect($scope.publishContent).toHaveBeenCalled();
+        })
+        describe('Open Request Changes & Publish checklist', function() {
+            it('When initPopup is called with api response available & reject popup is opened', function() {
+                $scope.checklistMode = "reject";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistJSON));
+                $scope.initPopup();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect($scope.checklistMode).toBeDefined();
+                expect(checklistConfig.reject.subtitle).toEqual(checklistJSON.data.result.form.data.fields[0].title);
+                expect(checklistConfig.reject.otherReason).toEqual(checklistJSON.data.result.form.data.fields[0].otherReason);
+                expect(checklistConfig.reject.contents).toEqual(checklistJSON.data.result.form.data.fields[0].contents);
+                expect($scope.checklistItems).toEqual(checklistConfig.reject);
+            })
+            it('When initPopup is called with api failed', function() {
+                $scope.checklistMode = "reject";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistJSON));
+                spyOn(ecEditor, 'dispatchEvent').and.callThrough();
+                $scope.initPopup();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect(ecEditor.dispatchEvent).toHaveBeenCalled();
+            })
+            it('When initPopup is called with empty api response & reject popup is opened', function() {
+                $scope.checklistMode = "reject";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistEmptyResponse));
+                spyOn(ecEditor, 'dispatchEvent').and.callThrough();
+                $scope.initPopup();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect(checklistConfig.reject.subtitle).toEqual(checklistConfig.reject.subtitle);
+                expect(checklistConfig.reject.contents).toEqual(undefined);
+            })
+            it('When initPopup is called with api response available & publish popup is opened', function() {
+                $scope.checklistMode = "publish";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistJSON));
+                $scope.initPopup();
+                expect($scope.checklistMode).toBeDefined();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect(checklistConfig.publish.subtitle).toEqual(checklistJSON.data.result.form.data.fields[0].title);
+                expect(checklistConfig.reject.contents).toEqual(checklistJSON.data.result.form.data.fields[0].contents);
+                expect($scope.checklistItems).toEqual(checklistConfig.publish);
+            })
+            it('When initPopup is called with empty api response & publish popup is opened', function() {
+                $scope.checklistMode = "publish";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistEmptyResponse));
+                spyOn($scope, 'onCheckboxSelect').and.callThrough();
+                $scope.initPopup();
+                expect($scope.checklistMode).toHaveBeenCalled();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect(checklistConfig.publish.subtitle).toEqual(checklistConfig.publish.subtitle);
+                expect(checklistConfig.publish.contents).toEqual(undefined);
+                expect($scope.checklistItems).toEqual(checklistConfig.publish);
+            })
+            it('When initPopup is called with & user opened to see the changes requested option', function() {
+                $scope.checklistMode = "";
+                spyOn(org.ekstep.services.metaService, 'getFormConfigurations').and.returnValue(Promise.resolve(checklistJSON));
+                spyOn($scope, 'onCheckboxSelect').and.callThrough();
+                $scope.initPopup();
+                expect($scope.checklistMode).toHaveBeenCalled();
+                expect(org.ekstep.services.metaService.getFormConfigurations).toHaveBeenCalled();
+                expect(checklistConfig.reject.otherReason).toEqual(checklistJSON.data.result.form.data.fields[0].otherReason);
+                expect(checklistConfig.reject.contents).toEqual(checklistJSON.data.result.form.data.fields[0].contents);
+                expect($scope.checklistItems).toEqual(checklistConfig.reject);
+                expect($scope.checklistItems.title).toEqual(checklistConfig.read.title);
+                expect($scope.checklistItems.subtitle).toEqual(checklistConfig.read.subtitle);
+            })
         })
 
     })
