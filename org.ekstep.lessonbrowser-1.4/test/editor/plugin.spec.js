e04ecc6fc1c07e359763697ff216cadec7004961
Gourav More <gourav_m@tekditechnologies.com>
2018-08-28 15:20:54 +0530

    Issue #SB-6241 fix: lesson browser testcases

--- org.ekstep.lessonbrowser-1.4/test/editor/plugin.spec.js
+++ org.ekstep.lessonbrowser-1.4/test/editor/plugin.spec.js
@@ -5,6 +5,9 @@ describe("lesson browser plugin", function() {
             manifest = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.lessonbrowser");
             path = ecEditor.resolvePluginResource(manifest.id, manifest.ver, "editor/lessonBrowserApp.js");
             pluginInstance = ecEditor.instantiatePlugin("org.ekstep.lessonbrowser");
+            var templatePath = ecEditor.resolvePluginResource(manifest.id, manifest.ver, "editor/lessonbrowser.html");
+            var controllerPath = ecEditor.resolvePluginResource(manifest.id, manifest.ver, "editor/lessonBrowserApp.js");
+            ecEditor.getService(ServiceConstants.POPUP_SERVICE).loadNgModules(templatePath, controllerPath);
             done();
         });
     });
@@ -19,7 +22,7 @@ describe("lesson browser plugin", function() {
             $ocLazyLoad.load([
                 { type: 'js', path: path }
             ]).then(function() {
-                ctrl = $controller("mainController", { $scope: $scope, instance: { manifest: manifest } });
+                ctrl = $controller("lessonController", { $scope: $scope, instance: { manifest: manifest } });
                 done();
             }, function(error) {
                 done();
@@ -30,15 +33,57 @@ describe("lesson browser plugin", function() {
         });
     });
     
-    describe("When plugin is initialized", function() {
+    describe("Lesson browser", function() {
     	it("It should initialized Lesson browser plugin", function() {
             spyOn(pluginInstance, 'initialize').and.callThrough();
             pluginInstance.initialize(manifest);
             console.log("manifest", manifest);
             console.log("pluginInstance", pluginInstance);
             expect(pluginInstance.initialize).toHaveBeenCalled();
-            expect(pluginInstance._templatePath).not.toBeUndefined();
-            expect(pluginInstance.controllerPath).not.toBeUndefined();
+        });
+        it("Should initialize the lesson browser without filters", function(done) {
+            spyOn($scope, "initPreview").and.callThrough();
+            ecEditor.dispatchEvent('org.ekstep.lessonbrowser:show', {
+                "filters": {
+                    "lessonType": [
+                    "Resource"
+                    ]
+                }
+            });
+            expect($scope.mainTemplate).toEqual("cardDetailsView");
+            done();
+        });
+        it("Should initialize the lesson browser without filters", function(done) {
+            spyOn($scope, "initPreview").and.callThrough();
+            ecEditor.dispatchEvent('editor:invoke:viewall', {
+                "client": "org",
+                "request": {
+                    "mode": "soft",
+                    "filters": {
+                    "objectType": [
+                        "Content"
+                    ],
+                    "status": [
+                        "Live"
+                    ],
+                    "contentType": [
+                        "Collection",
+                        "Resource",
+                        "Story",
+                        "Worksheet"
+                    ]
+                    },
+                    "offset": 0,
+                    "limit": 100,
+                    "softConstraints": {
+                    "gradeLevel": 100,
+                    "medium": 50,
+                    "board": 25
+                    }
+                },"callback": callback()
+            });
+            expect($scope.mainTemplate).toEqual("selectedResult");
+            done();
         });
     });
 });
