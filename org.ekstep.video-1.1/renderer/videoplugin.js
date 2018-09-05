94122a018c3efc256024f0a372191e715d99d275
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2018-09-05 14:57:23 +0530

    Issue #SB-5876 fix: Youtube in ECML content - test cases

--- org.ekstep.video-1.1/renderer/videoplugin.js
+++ org.ekstep.video-1.1/renderer/videoplugin.js
@@ -88,7 +88,7 @@ Plugin.extend({
     },
     checkValidYoutube: function (url) {
         var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-        if (url.match(p)) {
+        if (url && url.match(p)) {
             return url.match(p)[1];
         }
         return false;
