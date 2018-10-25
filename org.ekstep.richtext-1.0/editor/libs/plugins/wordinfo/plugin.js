0e12e1d2d1160ff0998d577389f8e3975bd4a755
Rajesh Gupta <rajesh.gupta@compassites.net>
2018-10-25 14:26:15 +0530

    Issue #SB-7210 fix:Added wordinfo with richtext

--- org.ekstep.richtext-1.0/editor/libs/plugins/wordinfo/plugin.js
+++ org.ekstep.richtext-1.0/editor/libs/plugins/wordinfo/plugin.js
@@ -19,26 +19,12 @@ CKEDITOR.plugins.add('wordinfo', {
             });
         }
         function openWordInfoDialog(e) {
-            //delete CKEDITOR.instances['editor1'];
-            // CKEDITOR.instances['editor1'].setData(instance)
-            // var manifest = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.richtext");
-            // CKEDITOR.replace( 'editor1', {
-            //     customConfig: ecEditor.resolvePluginResource(manifest.id, manifest.ver, "editor/libs/config.js"),
-            //     skin: 'moono-lisa,'+CKEDITOR.basePath + "skins/moono-lisa/",
-            //     contentsCss: ecEditor.resolvePluginResource(manifest.id, manifest.ver, "editor/libs/contents.css"),
-            // });
-            // var textObj = ecEditor.getCurrentObject();
-            // if(e.currentScope.ngDialogData && e.currentScope.ngDialogData.textSelected && textObj) {
-            //     ctrl.selectedText = true;
-            //     CKEDITOR.instances.editor1.setData(textObj.config.text);
-            // }
-            
             var instance = this;
             var text = CKEDITOR.instances.editor1.getData();
             if (text) {
-                var textObj = ecEditor.getCurrentObject();
+                var textObj = _.cloneDeep(ecEditor.getCurrentObject());
                 if (!textObj || textObj.manifest.id != this.type) {
-                    textObj = textObj;
+                    textObj = textObj = {'config': {}, 'attributes': {}}; 
                 }
                 textObj.config.text = CKEDITOR.instances.editor1.document.getBody().getText();
                 ecEditor.dispatchEvent('org.ekstep.wordinfobrowser:showpopup', {
@@ -51,37 +37,25 @@ CKEDITOR.plugins.add('wordinfo', {
         }
 
         function convertTexttoWordInfo(data, templateData) {
-            // var textObj =  {'config' : {}, 'attributes': {}};
-            // textObj.config.text = data.text;
-            // textObj.config.words = data.words;
-            // textObj.config.wordfontcolor = data.wordfontcolor;
-            // textObj.config.wordhighlightcolor = data.wordhighlightcolor;
-            // textObj.config.wordunderlinecolor = data.wordunderlinecolor;
-            // textObj.attributes.textType = 'wordinfo';
-            // instance.data = textObj;
-            // ecEditor.dispatchEvent("config:show");
-            // ecEditor.render();
-
-            var textObj = ecEditor.getCurrentObject();
-            var instance = ecEditor.getPluginInstance(textObj.id);
-            TextWYSIWYG.resetProperties(textObj); // This will reset all WYSIWYG properties of text instance for WordInfo.
-            textObj.data = templateData;
-            textObj.config.text = textObj.editorObj.text = data.text;
+            var textObj =  {'config' : {}, 'attributes': {}, 'addMedia': function(){}};
+            var manifest = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.richtext");
+            textObj.config.text = data.text;
             textObj.config.words = data.words;
             textObj.config.wordfontcolor = data.wordfontcolor;
             textObj.config.wordhighlightcolor = data.wordhighlightcolor;
             textObj.config.wordunderlinecolor = data.wordunderlinecolor;
             textObj.attributes.textType = 'wordinfo';
+            textObj.data = templateData;
             textObj.addMedia({
                 "id": "org.ekstep.text.popuptint",
-                "src": ecEditor.resolvePluginResource(instance.manifest.id, instance.manifest.ver, "assets/popuptint.png"),
+                "src": ecEditor.resolvePluginResource(manifest.id, manifest.ver, "assets/popuptint.png"),
                 "type": "image",
                 "assetId": "org.ekstep.text.popuptint"
             });
             ecEditor.dispatchEvent('org.ekstep.text:addWordInfo', textObj);
-            //instance.addWordinfoconfigManifest(textObj);
             ecEditor.dispatchEvent("config:show");
             ecEditor.render();
+
         }
     }
 });
