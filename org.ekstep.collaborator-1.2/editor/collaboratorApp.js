b95e9816c03ba25ed31c053c14753707ad8174ad
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-12-11 13:35:09 +0530

    Issue #SB-8813 feat: Added telemetry for the collaborator

--- org.ekstep.collaborator-1.2/editor/collaboratorApp.js
+++ org.ekstep.collaborator-1.2/editor/collaboratorApp.js
@@ -1,12 +1,17 @@
 
-angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
+angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply', 'angular-inview'])
     .controller('collaboratorCtrl', ['$scope', '$timeout', 'instance', '$filter', function ($scope, $timeout, instance, $filter) {
         var ctrl = this;
+        var inViewLogs = [];
         ctrl.defaultImage = ecEditor.resolvePluginResource(instance.manifest.id, instance.manifest.ver, "assets/default-avatar.jpg");
         ctrl.searchRes = { count: 0, content: [] };
 
         $scope.mode = ecEditor.getConfig('editorConfig').mode;
         $scope.contentId = ecEditor.getContext('contentId');
+        $scope.telemetry = {
+            "pluginid": instance.manifest.id,
+            "pluginver": instance.manifest.ver
+        };
         $scope.currentCollaborators = [];
         $scope.userSelection = []; // Selected User object
         $scope.selectedUsersId = [];
@@ -65,7 +70,7 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
                         } else {
                             if (res.data.result.response.content.length) {
                                 $scope.currentCollaborators = res.data.result.response.content;
-                                console.log("currentCOllaborators", $scope.currentCollaborators);
+                                console.log("currentCollaborators", $scope.currentCollaborators);
                                 $scope.userSelection = _.cloneDeep(res.data.result.response.content);
                             }
                             console.log('Response collaborators object list', $scope.currentCollaborators);
@@ -73,7 +78,7 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
                         $scope.loadTemplate();
                     });
                 } else {
-                    console.error('Unable to fetch collection hierarchy', res);
+                    console.error('Unable to fetch collection hierarchy');
                     $scope.loadTemplate();
                 }
             });
@@ -83,6 +88,8 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Loads template based on availability of the collaborators
          */
         $scope.loadTemplate = function () {
+            console.log('in load template', $scope.userSelection);
+
             if ($scope.userSelection.length) {
                 $scope.isAddCollaboratorPopup = false;
                 $scope.isLoading = false;
@@ -90,6 +97,7 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
             } else {
                 $scope.loadAllUsers();
             }
+            $scope.$safeApply();
         }
 
         $scope.loadAllUsers = function () {
@@ -99,7 +107,7 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
             searchBody.request.filters = {};
             searchService.userSearch(searchBody, function (err, res) {
                 if (err) {
-                    console.log('Unable to fetch All Users ', err);
+                    console.error('Unable to fetch All Users ', err);
                 } else {
                     $scope.usersList = res.data.result.response.content;
                     $scope.isLoading = false;
@@ -124,7 +132,12 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Removed existing collaborators
          */
         $scope.removeCollaborator = function (user, index) {
+            $scope.generateTelemetry({ type: 'click', subtype: 'remove', target: 'removeCollaborator', targetid: 'button-remove' });
+            console.log('Before removal', $scope.selectedUsersId);
             $scope.userSelection.splice(index, 1);
+            _.remove($scope.selectedUsersId, (userId) => userId === user.identifier);
+            console.log('After removal', $scope.selectedUsersId);
+
             console.log('After Removed Collaborator=>', $scope.userSelection);
         }
 
@@ -132,15 +145,14 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Updates collaborators
          */
         $scope.updateCollaborators = function () {
-
+            $scope.generateTelemetry({ type: 'click', subtype: 'update', target: 'updateCollaborator', targetid: 'button-done' });
             console.log('currentCollaborators', $scope.currentCollaborators);
             console.log('selectedUsersId', $scope.selectedUsersId);
 
             if (_.isEqual($scope.currentCollaborators.map(user => user.identifier).sort(), $scope.selectedUsersId.sort())) {
-                alert('Coming here');
                 $scope.closePopup();
             } else {
-                updateCollaboratorRequest.request.content.collaborators = $scope.selectedUsersId;
+                updateCollaboratorRequest.request.content.collaborators = _.uniq($scope.selectedUsersId);
 
                 var searchService = org.ekstep.contenteditor.api.getService(ServiceConstants.SEARCH_SERVICE);
                 searchService.updateCollaborators(ecEditor.getContext('contentId'), updateCollaboratorRequest, function (err, res) {
@@ -156,6 +168,8 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
 
         // Close the popup
         $scope.closePopup = function (pageId) {
+            inViewLogs = [];
+            $scope.generateTelemetry({ type: 'click', subtype: 'cancel', target: 'closePopup', targetid: 'button-cancel' });
             $scope.closeThisDialog();
         };
 
@@ -163,19 +177,28 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Opens up window to add multiple collaborators
          */
         $scope.showUsersList = function () {
-            if ($scope.showUsersList.length) {
+            if ($scope.usersList.length) {
                 $scope.isAddCollaboratorPopup = true;
                 $scope.noResultFound = false;
                 //$scope.userSelection = [];
                 //$scope.selectedUsersId = [];
+
                 $timeout(function () {
                     ecEditor.jQuery('.checkbox').checkbox();
+                });
+                ctrl.applyAllJquery();
+
+                angular.element(document).ready(function () {
+                    $timeout(function () {
+                        ecEditor.jQuery('.checkbox').checkbox();
+                    });
                 });
             } else {
                 $scope.isAddCollaboratorPopup = true;
                 $scope.isLoading = true;
                 $scope.loadAllUsers();
             }
+            $scope.$safeApply();
         }
 
         // apply all jquery after dom render
@@ -192,16 +215,18 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
         $scope.toggleSelectionUser = function (user) {
             var idx = $scope.selectedUsersId.indexOf(user.identifier);
             if (idx > -1) {
+                $scope.generateTelemetry({ type: 'click', subtype: 'uncheck', target: 'user', targetid: user.identifier });
                 ecEditor.jQuery('#checkBox_' + user.identifier + ' >.checkBox').prop('checked', false);
                 $scope.userSelection.splice(idx, 1); // is currently selected, remove from selection list
                 $scope.selectedUsersId.splice(idx, 1);
             } else {
+                $scope.generateTelemetry({ type: 'click', subtype: 'check', target: 'user', targetid: user.identifier });
                 ecEditor.jQuery('#checkBox_' + user.identifier + ' >.checkBox').prop('checked', true);
                 $scope.userSelection.push(user); // is newly selected, add to the selection list
                 $scope.selectedUsersId.push(user.identifier);
             }
 
-            console.log('currentCOllaborator', $scope.currentCollaborators);
+            console.log('currentCollaborator', $scope.currentCollaborators);
 
         }
 
@@ -209,9 +234,10 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Selects User and show check mark checked
          */
         $scope.selectUser = function (user) {
+            $scope.generateTelemetry({ type: 'click', subtype: 'select', target: 'user', targetid: user.identifier });
             $scope.userSelection.push(user); // is newly selected, add to the selection list
             $scope.selectedUsersId.push(user.identifier);
-            // $scope.returnSelectedUsers();
+            $scope.returnSelectedUsers();
         }
 
         // Add or Remove user
@@ -232,6 +258,8 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
 
             var searchService = org.ekstep.contenteditor.api.getService(ServiceConstants.SEARCH_SERVICE);
             searchBody.request.query = this.searchKeyword;
+            $scope.generateTelemetry({ type: 'click', subtype: 'submit', target: 'search', targetid: 'button-search' });
+
             searchService.userSearch(searchBody, function (err, res) {
                 if (err) {
                     console.log('User Search Failed:=>', err);
@@ -258,6 +286,7 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Shows Search Results in large screen
          */
         $scope.viewAllResults = function () {
+            $scope.generateTelemetry({ type: 'click', subtype: 'submit', target: 'viewAll', targetid: "" });
             $scope.usersList = ctrl.searchRes.content;
         }
 
@@ -266,7 +295,9 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
         }
 
         $scope.returnSelectedUsers = function () {
+            $scope.generateImpression({ type: 'click', subtype: 'submit', pageid: 'AddCollaborator' });
             $scope.isAddCollaboratorPopup = false;
+            inViewLogs = [];
         }
 
         /**
@@ -281,7 +312,43 @@ angular.module('collaboratorApp', ['ngTagsInput', 'Scope.safeApply'])
          * Generates telemetry
          */
         $scope.generateTelemetry = function (data) {
+            if (data) {
+                ecEditor.getService('telemetry').interact({
+                    "type": data.type,
+                    "subtype": data.subtype,
+                    "target": data.target,
+                    "targetid": data.targetid,
+                    "pluginid": $scope.telemetry.pluginid,
+                    "pluginver": $scope.telemetry.pluginver,
+                    "objectid": '',
+                });
+            }
         }
+
+        // Generate Impression telemetry
+        $scope.generateImpression = function (data) {
+            if (data) {
+                ecEditor.getService('telemetry').impression({
+                    "type": data.type,
+                    "subtype": data.subtype || "",
+                    "pageid": data.pageid || "",
+                    "uri": window.location.href,
+                    "visits": inViewLogs
+                });
+            }
+        }
+
+        $scope.lineInView = function (index, inview, item, section, pageSectionId) {
+            let obj = inViewLogs.filter((log) => log.identifier === item.identifier);
+            if (inview && !obj.length) {
+                inViewLogs.push({
+                    objid: item.identifier,
+                    section: section,
+                    index: index
+                });
+            }
+        }
+
         $scope.init();
     }]);
 
