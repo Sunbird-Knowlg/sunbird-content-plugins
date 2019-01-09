4973d75b4775f3e2ced194afe3276ef62f5835b9
Jagadish Pujari <jagadish.pujari@tarento.com>
2019-01-09 14:39:44 +0530

    Issue #000 feat: added newly updated plugins from QS

--- org.ekstep.question-1.0/editor/question-ctrl.js
+++ org.ekstep.question-1.0/editor/question-ctrl.js
@@ -22,10 +22,13 @@ angular.module('org.ekstep.question', ['org.ekstep.metadataform'])
 		questionbankPlugin: 'org.ekstep.questionbank',
     formElementId: '#questionMetaDataTemplate',
     metadataFormName: 'questionMetaDataTemplate'
-	};
-	$scope.questionData = {'max_score': 1};
-	$scope.questionData.isShuffleOption = false;
-	$scope.questionData.isPartialScore = true;
+  };
+  $scope.questionData = {
+    max_score: 1,
+    isShuffleOption: false,
+    isPartialScore: true,
+    evalUnordered: false
+  };
   $scope.templateIcons = [];
   _.each($scope.templatesType, function(template, key){
     var templateIconName = template.toLowerCase();
@@ -147,7 +150,7 @@ angular.module('org.ekstep.question', ['org.ekstep.metadataform'])
   $scope.setPreviewData = function () {
     var confData = {};
     var qObj = {
-      "config": '{"metadata":{"title":"question title","description":"question description","medium":"English"},"max_time":0,"max_score":' + $scope.questionData.max_score + ',"partial_scoring":' + $scope.questionData.isPartialScore + ',"isShuffleOption":' + $scope.questionData.isShuffleOption + ',"layout":' + JSON.stringify($scope.questionData.templateType) + '}',
+      "config": '{"metadata":{"title":"question title","description":"question description","medium":"English"},"max_time":0,"max_score":' + $scope.questionData.max_score + ',"partial_scoring":' + $scope.questionData.isPartialScore + ',"isShuffleOption":' + $scope.questionData.isShuffleOption + ',"layout":' + JSON.stringify($scope.questionData.templateType) + ',"evalUnordered":' + $scope.questionData.evalUnordered + '}',
       "data": JSON.stringify($scope.questionCreationFormData),
       "id": "c943d0a907274471a0572e593eab49c2",
       "pluginId": $scope.selectedTemplatePluginData.plugin.id,
@@ -269,7 +272,7 @@ angular.module('org.ekstep.question', ['org.ekstep.metadataform'])
         var metadataObj = $scope.questionMetaData;    
         metadataObj.category = $scope.category;
         // TODO: questionCount should be sent from unit template controllers. Currently it is hardcoded to 1.
-        data.config = { "metadata": metadataObj, "max_time": 0, "max_score": $scope.questionData.max_score, "partial_scoring": $scope.questionData.isPartialScore, "layout": $scope.questionData.templateType, "isShuffleOption" : $scope.questionData.isShuffleOption, "questionCount": 1};
+        data.config = { "metadata": metadataObj, "max_time": 0, "max_score": $scope.questionData.max_score, "partial_scoring": $scope.questionData.isPartialScore, "layout": $scope.questionData.templateType, "isShuffleOption" : $scope.questionData.isShuffleOption, "questionCount": 1, "evalUnordered": $scope.questionData.evalUnordered};
         data.media = $scope.questionCreationFormData.media;
         questionFormData.data = data;
         var metadata = {
@@ -375,6 +378,7 @@ angular.module('org.ekstep.question', ['org.ekstep.metadataform'])
   	$scope.questionData.isPartialScore = questionData1.data.config.partial_scoring;
   	$scope.questionData.gradeLevel = questionData1.data.config.metadata.gradeLevel;
   	$scope.questionData.isShuffleOption = questionData1.data.config.isShuffleOption;
+  	$scope.questionData.evalUnordered = questionData1.data.config.evalUnordered;
   	$scope.category = questionData.category;
   	if (questionData1.data.config.metadata.concepts) {
   		$scope.Totalconcepts = questionData1.data.config.metadata.concepts.length;
