57d26e18b8edb4c5eef2cc3e2f84d8ec423f67b4
loganathan1989 <loganathan.shanmugam@tareto.com>
2019-01-22 22:26:42 +0530

    Issue #000 feat: devcon appu

--- org.ekstep.appu-1.0/editor/command.js
+++ org.ekstep.appu-1.0/editor/command.js
@@ -3,24 +3,23 @@ var processOtherCommands = function(text){
 }
 var executeCommand = function(text){
     commands = [
-        {action: "get content", view: "portal", api: "search?_key_=_value_"},
-        {action: "get images", view: "editor"},
-        {action: "get audios", view: "editor"},
-        {action: "get questions", view: "editor"}
+        {action: "get content", view: "portal", event: ""},
+        {action: "get images", view: "editor", event: "openImageBrowser",type:'image'},
+        {action: "get audios", view: "editor", event: "openAudioBrowser",type:'audio'},
+        {action: "get questions", view: "editor" , event: "openQuestionBrowser"}
     ];
     text = text.toLowerCase();
     text = text.trim();
-    var viewCommands = _.map(commands, function(obj){ if(obj.view == "editor") return obj});
-    var editorActions = _.map(viewCommands, "action");
-    var action = _.forEach(editorActions, function(value) {
-        if(text.indexOf(value) >= 0){
-            return value;
-        }
-        
+    var selectedAction;
+    _.forEach(commands, function(command) {
+        if(text.indexOf(command.action) >= 0 && command.view == window.screenView){
+            selectedAction = command;
+            return false;
+        }        
       });
     //var validCommand =  _.indexOf(editorActions, text)
-    if(action){
-        var commandQuery = text.replace(action, '').trim(); //text.substr(_.lastIndexOf(editorActions, text), text.length);
+    if(selectedAction){
+        var commandQuery = text.replace(selectedAction.action, '').trim(); //text.substr(_.lastIndexOf(editorActions, text), text.length);
         _.each(['for', 'of'], function(value){ 
             if(_.indexOf(commandQuery, value)) {
                 commandQuery = commandQuery.replace(value, '').trim()
@@ -36,47 +35,19 @@ var executeCommand = function(text){
             value = queryParams[0]
             key = queryParams[1]
         }
-        hanldeQuery(action, key, value);
+        hanldeQuery(selectedAction, key, value);
     } else {
-        // show message, invalid command please try again
+        var evData = {message:'Invalid command'};
+        ecEditor.dispatchEvent('org.ekstep.appu:message',evData);
     }
-
-    
-
-    switch(action) {
-        
-     case 'hi':
-     case 'hello':
-     case 'hey':
-     case 'hi appu':
-     case 'hello appu':
-     case 'hey appu':
-     case 'appu':
-     {
-         console.log('Hi,This is Appu,What can i do for you....');
-     }
-     break;
-
-     case 'get all crow images':
-     ecEditor && ecEditor.dispatchEvent('org.ekstep.image:assetbrowser:open');
-     setTimeout(function(){         
-         $('#allImagesTab').click();
-         $("#searchAllImageAssets").val("crow");
-         $('#searchAllImageAssets').parent().children('i').click();
-     },3000);
-     break;
-
-     default:
-     console.log('Enter valid command');
-     break;
- } 
+ 
 }
 
-function hanldeQuery(action, key, value){
-    var api = _map(commands, function(obj){
-        if(obj.action == action) return obj.api;
-    });
-
-    api.replace('_key_', key);
-    api.replace('_value_', value);
+function hanldeQuery(item, key, value){
+    if(item && item.event != "" && value){
+        ecEditor.dispatchEvent('org.ekstep.appu:stopSpeechListener');
+        var evData = {key:key,value:value,item:item};
+        ecEditor.dispatchEvent('org.ekstep.appu:'+item.event,evData);
+        
+    }
 }
