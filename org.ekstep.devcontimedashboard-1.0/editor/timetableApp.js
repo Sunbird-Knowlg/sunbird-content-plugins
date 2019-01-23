78d652e6bb146e963bc10b11aca56726cf71fd3b
Rajesh Gupta <rajesh.gupta@compassites.net>
2019-01-23 11:03:31 +0530

    View topic details click on cell in table

--- org.ekstep.devcontimedashboard-1.0/editor/timetableApp.js
+++ org.ekstep.devcontimedashboard-1.0/editor/timetableApp.js
@@ -1,8 +1,9 @@
 
 angular.module('timetableApp', ['angular-inview'])
-    .controller('timetableCtrl', ['$scope', '$timeout', 'instance', function ($scope, $timeout, instance) {
+    .controller('timetableCtrl', ['$scope','$http', '$timeout', 'instance', function ($scope,$http, $timeout, instance) {
         
         $scope.isLoading = true;
+        $scope.detailsView = [];
         $scope.userSearchBody = {
             "filters":{
                 "objectType":["Content","AssessmentItem"],
@@ -15,118 +16,13 @@ angular.module('timetableApp', ['angular-inview'])
             "sort_by":{"lastUpdatedOn":"desc"}
         };
 
-        $scope.topics = {
-            "topic1": {
-              "dc_practice": [
-                {
-                  "identifier": "do_1126520449306705921230",
-                  "name": "topic 1 paracice",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic1"
-                  ],
-                  "keywords": [
-                    "dc_practice"
-                  ]
-                }
-              ],
-              "dc_assessment": [
-                {
-                  "identifier": "do_1126520449306705921230",
-                  "name": "1.2.1 Methods",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic1"
-                  ],
-                  "keywords": [
-                    "dc_assessment"
-                  ]
-                },
-                {
-                  "identifier": "do_1126520449306705921230",
-                  "name": "topic 1 dc_assessment",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic1"
-                  ],
-                  "keywords": [
-                    "dc_assessment"
-                  ]
-                }
-              ],
-              "dc_primary": [
-                {
-                  "identifier": "do_1126520449306705921230",
-                  "name": "1.2.1 Methods",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic1"
-                  ],
-                  "keywords": [
-                    "dc_primary"
-                  ]
-                }
-              ],
-              "dc_preparatory": [
-                {
-                  "identifier": "do_1126520449306705921230",
-                  "name": "1.2.1 Methods",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic1"
-                  ],
-                  "keywords": [
-                    "dc_preparatory"
-                  ]
-                }
-              ]
-            },
-            "topic2": {
-              "dc_preparatory": [
-                {
-                  "identifier": "do_1126520449306705921229",
-                  "name": "topic2 dc_preparatory",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic2"
-                  ],
-                  "keywords": [
-                    "dc_preparatory"
-                  ]
-                }
-              ]
-            },
-            "topic5": {
-              "dc_preparatory": [
-                {
-                  "identifier": "do_1126520449306705921229",
-                  "name": "topic 5 dc_preparatory",
-                  "objectType": "Content",
-                  "topic": [
-                    "topic5"
-                  ],
-                  "keywords": [
-                    "dc_preparatory"
-                  ]
-                }
-              ]
-            },
-            "topic4": {}
-          }
-
-          $scope.records = [
-            "Alfreds Futterkiste",
-            "Berglunds snabbköp",
-            "Centro comercial Moctezuma",
-            "Ernst Handel",
-          ]
-
         $scope.showDetailedView = false;
         $scope.topic;
         $scope.type;
         $scope.flag = false;
   
-        $scope.userDetails = ['Class 1', 'Geography'];
+        $scope.classDetails = ["Class 1"];
+        $scope.subjedDetails = ["Physics"];
 
         $scope.init = function () {
             $scope.contentNotFoundImage = ecEditor.resolvePluginResource(instance.manifest.id, instance.manifest.ver, "assets/content_not_found.jpg");
@@ -134,27 +30,58 @@ angular.module('timetableApp', ['angular-inview'])
             $scope.contentId = ecEditor.getContext('contentId');
             $scope.userService = org.ekstep.contenteditor.api.getService(ServiceConstants.USER_SERVICE);
             $scope.contentService = ecEditor.getService(ServiceConstants.CONTENT_SERVICE);
+            $scope.getteacherProfile();
             $scope.getContentTimetable();
         }
 
-        $scope.getContentTimetable = function () {
-        
+        /**
+         *  Call Api and get Teacher Details
+         *
+        */
+        $scope.getteacherProfile = function() {
+
         }
 
+        $scope.getContentTimetable = function () {
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json'
+                }
+            }
+            var  data =  {"request": { 
+                "filters":{
+                    "objectType":["Content"],
+                    "status":[],
+                    "subject":"evs",
+                    "keywords":["dc_primary","dc_secondary","dc_preparatory","dc_practice","dc_assessment"]
+                },
+                "exists":["keywords"],
+                "limit":50,
+                "fields":["identifier","name","topic","keywords"],
+                "sort_by":{"lastUpdatedOn":"desc"}
+                }
+            }
+            var url = 'https://dev.ekstep.in/api/devcon/v3/search/table';
+            $http.post(url, data, config)
+            .then(function(response) {
+                $scope.topic2 = response;
+            }, function(err){
+                console.log(err);
+            }
+        );
+        
+    }
+
+        /**
+         * This method is used for show details view of table content when click on number count
+         */
         $scope.showDetailedRow = function(type, topic) {
-            if ($scope.topic && $scope.type && ($scope.topic === topic && $scope.type === type)) {
-              $scope.flag = !$scope.flag;
-            } else {
-              $scope.flag = true;
-            }
-            console.log($scope.flag);
-            $scope.topic = topic;
-            $scope.type = type;
-            if ($scope.flag) {
-              $scope.showDetailedView = true;
-            } else {
-              $scope.showDetailedView = false;
-            }
+            $scope.detailsView = [];
+            type.forEach(function(data){
+                $scope.detailsView.push(data.name);
+            });
+            $scope.topicTitle = topic;
+            $scope.showDetailedView = !$scope.showDetailedView;
         }
 
 
