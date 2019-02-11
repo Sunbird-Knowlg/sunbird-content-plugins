dc3c083ec8efcd42baa4b1d0442435f4cf70b122
Sivashanmugam Kannan <sivashanmugam.kannan@funtoot.com>
2019-02-11 12:16:10 +0530

    Issue #SB-6814, Fix : Change of Variable name in renderer side

--- org.ekstep.questionunit.ftb-1.0/renderer/ftbcontroller.js
+++ org.ekstep.questionunit.ftb-1.0/renderer/ftbcontroller.js
@@ -15,7 +15,7 @@ FTBController.question = undefined;
 
 FTBController.getQuestionTemplate = function () {
   return '<div id="ftb-template" class="plugin-content-container">\
-    <div class="qs-ftb-container <% if(question.data.question.isRTL) { %> urdu-support <% } %>" >\
+    <div class="qs-ftb-container <% if(question.data.question.isUrdu) { %> urdu-support <% } %>" >\
       <div class="qs-ftb-content">\
           <div class="qs-ftb-question" id="qs-ftb-question">\
           <% if ( question.data.question.image.length > 0 ){ %> \
