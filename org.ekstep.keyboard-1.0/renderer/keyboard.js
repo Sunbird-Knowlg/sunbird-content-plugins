e5337cfe2a03635315fdcc2a709d0a89d5ef5be0
Jagadish Pujari <jagadish.pujari@tarento.com>
2019-02-12 17:27:11 +0530

    Issue #SB-7813 fix: New UI and support mulitlanguage more than 26 character

--- org.ekstep.keyboard-1.0/renderer/keyboard.js
+++ org.ekstep.keyboard-1.0/renderer/keyboard.js
@@ -13,40 +13,30 @@ Keyboard.constant = {
 };
 
 Keyboard.htmlLayout = '<div id = "keyboardDiv">\
-    <div class="textBoxArea">\
-      <input type="text" id="keyboardInput" class="ansField" placeholder="Enter answer"  onclick="Keyboard.logTelemetryInteract(event);" disabled autofocus />\
-    </div>\
     <div id="keyboard" class="keyboardArea">\
+    <div class="textBoxArea">\
+        <input type="text" id="keyboardInput" class="ansField" placeholder="Enter answer"  onclick="Keyboard.logTelemetryInteract(event);" disabled autofocus />\
+      </div>\
         <div class="parentDivMainKeyboard qc-keyboard-bottom">\
-          <% if(Keyboard.buttons.firstRow.length >0) { %> \
+          <% if(Keyboard.buttons.length >0) { %> \
             <div id="firstRowAlpha">\
-              <% _.each(Keyboard.buttons.firstRow, function(but) { %> \
-                <div onclick="Keyboard.addLetter(event);" class="key_barakhadi" style="width:<%= Keyboard.keyWidth %>%">\
+              <% _.each(Keyboard.buttons, function(but) { %> \
+                <div onclick="Keyboard.addLetter(event);" class="key_barakhadi">\
                   <span><%= but %>\</span>\
                 </div>\
               <% }); %>\
             </div>\
-            <div id="secondRowAlpha">\
-              <% _.each(Keyboard.buttons.secondRow, function(but) { %> \
-                <div onclick="Keyboard.addLetter(event);" class="key_barakhadi" style="width:<%= Keyboard.keyWidth %>%">\
-                  <span><%= but %>\</span>\
-                </div>\
-              <% }); %>\
-            </div>\
-            <div class="erasedDivParent">\
-                <img src=<%=Keyboard.addImageIcon("renderer/assets/eras_icon.png") %> class="qc-erase-icon" onclick="Keyboard.deleteText();" />\
-            </div>\
-            <% if(Keyboard.buttons.length > 10) { %> \
-              <div id="secondRowdiv"></div>\
-            <% } %> \
             <div id="thirdRowAlpha">\
                 <div class="special_keys" onclick="Keyboard.changeToNumeric()" style="font-size: 2vw;"><span>123</span></div>\
                 <div class="spaceBar" onclick="Keyboard.addLetter(event);" style=" font-size:3vw;"><span> </span></div>\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);"><span>,</span></div>\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);"><span>.</span></div>\
-            </div>\
-            <div class="hideKeyboard">\
-                <img src=<%=Keyboard.addImageIcon("renderer/assets/keyboard.svg") %> onclick="Keyboard.hideKeyboard();Keyboard.logTelemetryInteract(event);" />\
+                <div class="erasedDivParent">\
+                  <img src=<%=Keyboard.addImageIcon("renderer/assets/eras_icon.png") %> class="qc-erase-icon" onclick="Keyboard.deleteText();" />\
+                </div>\
+                <div class="hideKeyboard">\
+                  <img src=<%=Keyboard.addImageIcon("renderer/assets/keyboard.svg") %> onclick="Keyboard.hideKeyboard();Keyboard.logTelemetryInteract(event);" />\
+                </div>\
             </div>\
           <% } %> \
         </div>\
@@ -63,8 +53,6 @@ Keyboard.htmlLayout = '<div id = "keyboardDiv">\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>9</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>0</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>@</span></div>\
-            </div>\
-            <div id="secondRowNum">\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>?</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>!</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>%</span></div>\
@@ -77,21 +65,21 @@ Keyboard.htmlLayout = '<div id = "keyboardDiv">\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>÷</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>×</span></div>\
                 <div class="key_barakhadi_numeric" onclick="Keyboard.addLetter(event);"><span>=</span></div>\
-                <div class="erasedDivParent">\
-                    <img src=<%=Keyboard.addImageIcon("renderer/assets/eras_icon.png") %> class="qc-erase-icon" onclick="Keyboard.deleteText()" />\
-                </div>\
             </div>\
             <div class="third-row-numeric">\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);" id="<_btn"><span>&lt;</span></div>\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);" id=">_btn"><span>&gt;</span></div>\
-                <div>\
+                <div style="margin-left: 3%;">\
                     <img src=<%=Keyboard.addImageIcon("renderer/assets/language_icon.png") %> class="qc-language-icon" onclick="Keyboard.changeToAlphabet()" /> </div>\
                 <div class="spaceBar" onclick="Keyboard.addLetter(event);" style=" font-size:3vw;"><span> </span></div>\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);"><span>,</span></div>\
                 <div class="special_keys" onclick="Keyboard.addLetter(event);"><span>.</span></div>\
-            </div>\
+                <div class="erasedDivParent">\
+                <img src=<%=Keyboard.addImageIcon("renderer/assets/eras_icon.png") %> class="qc-erase-icon" onclick="Keyboard.deleteText();" />\
+              </div>\
             <div class="hideKeyboard">\
               <img src=<%=Keyboard.addImageIcon("renderer/assets/keyboard.svg") %> onclick="Keyboard.hideKeyboard();Keyboard.logTelemetryInteract(event);" />\
+            </div>\
             </div>\
         </div>\
     </div>\
@@ -102,10 +90,10 @@ Keyboard.createKeyboard = function(customButtons) {
   customButtons = customButtons.split(',');
   customButtons = _.uniq(customButtons);
   Keyboard.buttons = customButtons.splice(0, customButtons.length);
-  var splitButtonto = parseInt(Keyboard.buttons.length / 2);
-  Keyboard.buttons.firstRow = Keyboard.buttons.slice(0, splitButtonto);
-  Keyboard.buttons.secondRow = Keyboard.buttons.slice(splitButtonto, Keyboard.buttons.length);
-  Keyboard.keyWidth = parseInt(100 / Keyboard.buttons.secondRow.length);
+  // var splitButtonto = parseInt(Keyboard.buttons.length / 2);
+  // Keyboard.buttons.firstRow = Keyboard.buttons.slice(0, splitButtonto);
+  // Keyboard.buttons.secondRow = Keyboard.buttons.slice(splitButtonto, Keyboard.buttons.length);
+  // Keyboard.keyWidth = parseInt(100 / Keyboard.buttons.secondRow.length);
 };
 
 Keyboard.changeToNumeric = function() {
