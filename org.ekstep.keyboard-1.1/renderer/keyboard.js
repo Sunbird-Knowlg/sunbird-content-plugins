b127b1e6f576a722f9126a81705a864d68ddab45
Jagadish Pujari <jagadish.pujari@tarento.com>
2019-02-25 17:49:12 +0530

    Issue #SB-7813 fix: 1280 X 720 resolution custom keyboard alignement fixed

--- org.ekstep.keyboard-1.1/renderer/keyboard.js
+++ org.ekstep.keyboard-1.1/renderer/keyboard.js
@@ -13,22 +13,22 @@ Keyboard.constant = {
 };
 
 Keyboard.htmlLayout = '<div id = "keyboardDiv">\
-    <div id="keyboard" class="keyboardArea" style="height: 100%;">\
     <div class="textBoxArea">\
-        <input type="text" id="keyboardInput" class="ansField" placeholder="Enter answer"  onclick="Keyboard.logTelemetryInteract(event);" disabled autofocus />\
-      </div>\
+      <input type="text" id="keyboardInput" class="ansField" placeholder="Enter answer"  onclick="Keyboard.logTelemetryInteract(event);" disabled autofocus />\
+    </div>\
+    <div id="keyboard" class="keyboardArea" style="height: 100%;">\
         <div class="parentDivMainKeyboard qc-keyboard-bottom">\
           <% if(Keyboard.buttons.length >0) { %> \
             <div id="firstRow">\
-              <% _.each(Keyboard.buttons, function(but) { %> \
+              <% _.each(Keyboard.buttons, function(btn) { %> \
                 <div onclick="Keyboard.addLetter(event);" class="key_barakhadi">\
-                  <span><%= but %>\</span>\
+                  <span><%= btn %>\</span>\
                 </div>\
               <% }); %>\
             </div>\
             <div id="thirdRow">\
                 <div class="key_barakhadi" onclick="Keyboard.changeToNumeric()"><span>123</span></div>\
-                <div class="key_barakhadi spaceBar" onclick="Keyboard.addLetter(event);"><span> </span></div>\
+                <div class="key_barakhadi spaceBar" onclick="Keyboard.addLetter(event);"><span>&nbsp </span></div>\
                 <div class="key_barakhadi" onclick="Keyboard.addLetter(event);"><span>,</span></div>\
                 <div class="key_barakhadi" onclick="Keyboard.addLetter(event);"><span>.</span></div>\
                 <div class="erase-icon">\
@@ -72,7 +72,7 @@ Keyboard.htmlLayout = '<div id = "keyboardDiv">\
                 <div style="width: 7%; padding:1%">\
                   <img src=<%=Keyboard.addImageIcon("renderer/assets/language_icon.png") %> class="" onclick="Keyboard.changeToAlphabet()" style="width: 100%;" />\
                 </div>\
-                <div class="key_barakhadi spaceBar" onclick="Keyboard.addLetter(event);" ><span> </span></div>\
+                <div class="key_barakhadi spaceBar" onclick="Keyboard.addLetter(event);" ><span>&nbsp</span></div>\
                 <div class="key_barakhadi" onclick="Keyboard.addLetter(event);"><span>,</span></div>\
                 <div class="key_barakhadi" onclick="Keyboard.addLetter(event);"><span>.</span></div>\
                 <div class="erase-icon">\
