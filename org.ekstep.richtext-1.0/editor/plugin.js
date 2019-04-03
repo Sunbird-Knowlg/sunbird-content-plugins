1153d3bb00fcc64fd7733858685d45c7d5c4714b
Gourav More <gourav_m@tekditechnologies.com>
2019-04-03 20:01:50 +0530

    Issue #SB-9649 fix: Rich text layout is getting changed

--- org.ekstep.richtext-1.0/editor/plugin.js
+++ org.ekstep.richtext-1.0/editor/plugin.js
@@ -89,11 +89,10 @@ org.ekstep.contenteditor.basePlugin.extend({
                });
                editorObject.attributes.w =  e.target.getWidth();
                editorObject.attributes.h =  e.target.getHeight();
+               editorObject.editorObj.width = e.target.getWidth();
+               editorObject.editorObj.height = e.target.getHeight();
                ecEditor.jQuery("#" + e.target.id).width(e.target.getWidth());
                ecEditor.jQuery("#" + e.target.id).height(e.target.getHeight());
-               
-               /*Hardcoded plugin name - We can not get plugin instance here */
-               ecEditor.dispatchEvent("org.ekstep.richtext:adddiv", { data: editorObject });
         }
     },
 
