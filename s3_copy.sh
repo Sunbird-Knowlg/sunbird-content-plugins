3f0bdfc6f8bfdae08579cdd19aebf6fd28b4e180
Sivashanmugam Kannan <sivashanmugam.kannan@funtoot.com>
2019-05-07 07:34:38 +0530

    Issue #SB-12092, Fix : S3_COPY and AZURE copy update

--- s3_copy.sh
+++ s3_copy.sh
@@ -84,7 +84,9 @@ declare -a coreplugins=("org.ekstep.activitybrowser-1.3"
                 "org.ekstep.questionunit.ftb-1.0"
                 "org.ekstep.questionunit.ftb-1.1"
                 "org.ekstep.questionunit.mtf-1.2"
+                "org.ekstep.questionunit.reorder-1.0"
                 "org.ekstep.questionunit.reorder-1.1"
+                "org.ekstep.questionunit.sequence-1.0"
                 "org.ekstep.questionunit.sequence-1.1");
 
 for i in "${coreplugins[@]}"
