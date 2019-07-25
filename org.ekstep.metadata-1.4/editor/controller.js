1b191e6841deb930d2231c647c4a794cdb97c254
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-07-25 18:42:27 +0530

    Issue #SB-13969 fix: metadata plugin issue

--- org.ekstep.metadata-1.4/editor/controller.js
+++ org.ekstep.metadata-1.4/editor/controller.js
@@ -608,6 +608,15 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
             $scope.$safeApply();
         });
     }
+
+    $scope.displayValue = function(value){
+        if(_.isArray(value)){
+            return value.length > 0 ? value.join(', ') : '--';
+        }else{
+            return _.isEmpty(value) ? '--' : value;
+        }
+    }
+
     $scope.init()
 
 }]);
