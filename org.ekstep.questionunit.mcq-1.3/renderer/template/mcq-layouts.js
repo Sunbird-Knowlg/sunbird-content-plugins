ab9af556b5d7f90ca09ede2b7f64e2a010022f99
Jagadish Pujari <jagadish.pujari@tarento.com>
2019-08-22 15:17:44 +0530

    Issue #SB-14370 fix: Question image resize

--- org.ekstep.questionunit.mcq-1.3/renderer/template/mcq-layouts.js
+++ org.ekstep.questionunit.mcq-1.3/renderer/template/mcq-layouts.js
@@ -317,7 +317,7 @@ MCQController.vertical2.getQuestionTemplate = function (question) {
     var qTemplate = "<div class='mcq-qLeft-question-container'>\
     <div class='mcq-question-image'>\
                 <% if(question.data.question.image){%>\
-                <div class='image-container border-solid mcq-qleft-img-container'>\
+                <div class='image-container mcq-qleft-img-container'>\
                 <img class='q-image' onclick='MCQController.showImageModel(event, <%=MCQController.pluginInstance.getAssetUrl( question.data.question.image) %>)'\ src=<%=MCQController.pluginInstance.getAssetUrl( question.data.question.image) %> />\
                 <img onclick=MCQController.zoomImage('<%=question.data.question.image%>') class='zoom-image' src=<%= MCQController.pluginInstance.getDefaultAsset('"+zoomIcon+"') %>>\
                 </div>\
@@ -406,10 +406,8 @@ MCQController.vertical2.postRender = function (question) {
 
 MCQController.onImageDomLoad = function (container, question) {
     var domLoaded = false;
-    if ($(".mcq-option-img").length){
         $('.mcq-option-img').on("load", function () {
             if(domLoaded) return;
-
             var defaultMaxHeight = $(container).outerHeight();
             var defaultMaxWidth = $(container).width();
             var size = MCQController.getMaxWidthHeight(question.data.options, defaultMaxHeight, defaultMaxWidth);
@@ -417,13 +415,13 @@ MCQController.onImageDomLoad = function (container, question) {
             $(".border-solid").css("width", size.maxWidth);
             domLoaded = true;
         })
-    }else{
-        var defaultMaxHeight = $(container).outerHeight();
-        var defaultMaxWidth = $(container).width();
-        var size = MCQController.getMaxWidthHeight(question.data.options, defaultMaxHeight, defaultMaxWidth);
-        $(".border-solid").css("height", size.maxHeight);
-        $(".border-solid").css("width", size.maxWidth);
-    }
+        
+        $('.q-image').on("load", function () {
+            var defaultMaxHeight = $(container).outerHeight();
+            var defaultMaxWidth = $(container).width();
+            $(".mcq-qleft-img-container").css("height", defaultMaxHeight);
+            $(".mcq-qleft-img-container").css("width", defaultMaxWidth);
+        })
 }
 
 MCQController.grid.postRender = function (question) {
