841d8c4c80dedc33bf45e4e4c86036b938114689
JagadishPujari <jagadish.pujari@tarento.com>
2019-10-23 11:45:30 +0530

    Issue #SB-15111 feat: Summary page css fixes

--- org.ekstep.summary-1.0/renderer/summary-template.js
+++ org.ekstep.summary-1.0/renderer/summary-template.js
@@ -7,15 +7,15 @@ summaryTemplate.showTemplate = function(){
 summaryTemplate.getTemplate = function () {
     var summaryTemplateHTML = '<div style="width: 100%;position: absolute;top: 14%;left: 0px;height: 100%;">\
     <div class="assessment-content assessment-result-content">\
-    <div class="assessment-content-title mt-32">Submit your assessment to check your score.</div>\
+    <div class="assessment-content-title">Submit your assessment to check your score.</div>\
     <div class="assessment-content-description mt-16">\
       <div class="text-center">\
         <div>Total questions: <span class="score"><%= summaryTemplate._QSSummary.attempted.length +  summaryTemplate._QSSummary.skipped.length%></span></div>\
-        <div>Questions attempted: <span class="score"><%= summaryTemplate._QSSummary.attempted.length %></span></div>\
-        <div>Questions skipped: <span class="score"><%= summaryTemplate._QSSummary.skipped.length %></span></div>\
+        <div class="pt-8">Questions attempted: <span class="score"><%= summaryTemplate._QSSummary.attempted.length %></span></div>\
+        <div class="pt-8">Questions skipped: <span class="score"><%= summaryTemplate._QSSummary.skipped.length %></span></div>\
       </div>\
     </div>\
-    <div class="assessment-action-buttons mt-40">\
+    <div class="assessment-action-buttons mt-32">\
       <button type="submit" class="sb-btn sb-btn-primary sb-btn-normal sb-btn-responsive" onclick=summaryTemplate.pluginInstance.submitSummary()> Submit </button>\
     </div>\
     </div>\
