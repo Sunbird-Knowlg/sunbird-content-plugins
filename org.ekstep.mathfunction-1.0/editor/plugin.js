1fc2fb8cdc4d9d4f000d6f75cf79e903bf92bf47
dhirenaade <dhirenaade@gmail.com>
2020-05-08 14:05:06 +0530

    Issue #SB-18437 fix: katex plugin changes.

---
+++ org.ekstep.mathfunction-1.0/editor/plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @class  org.ekstep.mathfunction.EditorPlugin
+ */
+org.ekstep.mathfunction.EditorPlugin = org.ekstep.contenteditor.basePlugin.extend({
+
+    /**
+     * @memberOf org.ekstep.mathfunction.EditorPlugin#
+     */
+    newInstance: function() {}
+});
+//# sourceURL=mathfunctionEditorPlugin.js
---
+++ org.ekstep.mathfunction-1.0/test/editor/plugin.spec.js
@@ -0,0 +1,15 @@
+describe("EditorPlugin", function() {
+    describe("newInstance", function() {
+        var plugin;
+
+        beforeEach(function() {
+            plugin = new org.ekstep.mathfunction.EditorPlugin({}, {}, {});
+        });
+
+        it("should ?", function() {
+            plugin.newInstance();
+
+            expect(true).toBe(true);
+        });
+    });
+});
