aff1f43b27d5d18b85158060736610591dac9a7b
Gourav More <gourav_m@tekditechnologies.com>
2020-05-16 17:51:10 +0530

    Issue #SB-19291 fix: Question field is Blank when text and equation is added for MCQ and MTF type questions in the content editor preview

--- org.ekstep.questionunit-1.1/editor/plugin.js
+++ org.ekstep.questionunit-1.1/editor/plugin.js
@@ -113,10 +113,8 @@ org.ekstep.contenteditor.questionUnitPlugin = org.ekstep.contenteditor.basePlugi
             data.text = $(element).prop('outerHTML');
           }
           return data.text;
-      }else if(index == -1){
-        return data.text.replace(/<p>/g, "<p style='font-size:1.285em;'>");
       }else{
-        return data.text;
+        return data.text.replace(/<p>/g, "<p style='font-size:1.285em;'>");
       }
   }
 });
