5276f1a621331bfcc9e4e0d267e6728451e7251f
“Rajnish <rajnish_d@techjoomla.com>
2020-05-19 12:45:07 +0530

    Issue #SB-19266 fix : Page automatically gets scrolled up when user clicks on the text box for FTB type questions

--- org.ekstep.questionunit.ftb-1.1/renderer/ftbcontroller.js
+++ org.ekstep.questionunit.ftb-1.1/renderer/ftbcontroller.js
@@ -74,13 +74,13 @@ FTBController.invokeKeyboard = function (event) { // eslint-disable-line no-unus
   /*istanbul ignore else*/
   if (!(isbrowserpreview && (_.isUndefined(FTBController.question.data.question.keyboardConfig) || FTBController.question.data.question.keyboardConfig.keyboardType == "Device"))) { // eslint-disable-line no-undef
     $(FTBController.constant.qsFtbContainer).addClass("align-question");
+    setTimeout(function(){
+      document.querySelector('.highlightInput').scrollIntoView({ behavior: 'smooth' });
+    }, 200)
   }
   $('.ans-field').removeClass("highlightInput");
   var target = $('#' + event.target.id);
   target.addClass("highlightInput");
-  setTimeout(function(){
-    document.querySelector('.highlightInput').scrollIntoView({ behavior: 'smooth' });
-  }, 200)
   EkstepRendererAPI.dispatchEvent(FTBController.constant.keyboardPlugin + ":invoke", keyboardConfig, FTBController.keyboardCallback);
 };
 
