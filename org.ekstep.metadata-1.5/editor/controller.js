4e79c28ca4125694f30d4cbdcee1e045d016d634
“Rajnish <rajnish_d@techjoomla.com>
2020-06-02 18:52:08 +0530

    Issue #SB-10054 fix : User not able to remove filters.

--- org.ekstep.metadata-1.5/editor/controller.js
+++ org.ekstep.metadata-1.5/editor/controller.js
@@ -74,7 +74,7 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
     /**
     * @property         - Flag for checking clear fliter is clicked or not.
     */
-    $scope.clearFilterClick = false;
+    $scope.clearFilter = false;
 
     /**
      * @description          - Which is used to dispatch an event.
@@ -131,12 +131,10 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
             $scope.contentMeta[object.field.code] = (object.value === 'false') ? false : true;
         }
 
-        if($scope.clearFilterClick)
+        if($scope.clearFilter)
         {
              _.forEach($scope.contentMeta, function(value, key) {
-                if (JSON.stringify(value) === JSON.stringify([""])) {
-                    $scope.contentMeta[key] = [];
-                }
+                $scope.contentMeta[key] = (_.size(_.filter(value)) > 0) ? $scope.contentMeta[key] : []
             });
         }
         
@@ -439,7 +437,7 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
      */
     $scope.clearFilters = function(){
         setTimeout(function(){
-            $scope.clearFilterClick = true;
+            $scope.clearFilter = true;
             $('.dropdown').dropdown('clear');
             $scope.filterButtonVisibility= 0;
             $scope.$safeApply();
